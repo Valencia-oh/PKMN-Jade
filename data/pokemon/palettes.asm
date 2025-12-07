e60f8ab0e32ba4db9003790ac1c3ab7e69666c3b
Valencia-oh <valencia_oh@protonmail.com>
2025-12-06 22:27:02 -0500

    Update palettes.asm

--- data/pokemon/palettes.asm
+++ data/pokemon/palettes.asm
@@ -535,5 +535,7 @@ INCBIN "gfx/pokemon/ho_oh/normal.gbcpal", middle_colors
 INCLUDE "gfx/pokemon/ho_oh/shiny.pal"
 INCBIN "gfx/pokemon/celebi/normal.gbcpal", middle_colors
 INCLUDE "gfx/pokemon/celebi/shiny.pal"
+INCBIN "gfx/pokemon/falinks/normal.gbcpal", middle_colors
+INCLUDE "gfx/pokemon/falinks/shiny.pal"
 
 	assert_table_length NUM_POKEMON + 1
