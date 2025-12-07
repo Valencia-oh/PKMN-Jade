70073cc4525c83023a5ac84552c7b5545addb10e
Valencia-oh <valencia_oh@protonmail.com>
2025-12-07 16:44:11 -0500

    Update menu_icon_pals.asm

--- data/pokemon/menu_icon_pals.asm
+++ data/pokemon/menu_icon_pals.asm
@@ -262,5 +262,6 @@ MonMenuIconPals:
 	icon_pals RED,    BROWN  ; HO_OH
 	icon_pals GREEN,  PINK   ; CELEBI
 	icon_pals RED,    GREEN  ; APPLIN
+	icon_pals RED,    GREEN  ; APPLETUN
 	icon_pals RED,    BROWN  ; FALINKS
 	assert_table_length NUM_POKEMON
