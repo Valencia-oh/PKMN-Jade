48eff611b7f2aec5dbb0f7f7296037b23cc2c896
Valencia-oh <valencia_oh@protonmail.com>
2025-12-07 09:32:41 -0500

    Update falinks.asm

--- data/pokemon/dex_entries/falinks.asm
+++ data/pokemon/dex_entries/falinks.asm
@@ -1,10 +1,10 @@
-+	db "FORMATION@" ; species name
-+	dw 910, 1376 ; height, weight
-+
-+	db   "The brass which is"
-+	next "the one who stands,"
-+	next "in front issuing"
-+
-+	page "orders is stronger"
-+	next "and smarter than"
-+	next "its comrades.@"
+	db "FORMATION@" ; species name
+	dw 910, 1376 ; height, weight
+
+	db   "The brass which is"
+	next "the one who stands,"
+	next "in front issuing"
+
+	page "orders is stronger"
+	next "and smarter than"
+	next "its comrades.@"
