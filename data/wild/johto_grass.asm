066b70a75580df6b469b5068439c0f9fd6a0f46b
Valencia-oh <valencia_oh@protonmail.com>
2025-12-07 08:57:46 -0500

    Update johto_grass.asm

--- data/wild/johto_grass.asm
+++ data/wild/johto_grass.asm
@@ -1237,13 +1237,13 @@ JohtoGrassWildMons:
 	def_grass_wildmons ROUTE_29
 	db 10 percent, 10 percent, 10 percent ; encounter rates: morn/day/nite
 	; morn
-	dbw 2, PIDGEY
-	dbw 2, SENTRET
-	dbw 3, PIDGEY
-	dbw 3, SENTRET
-	dbw 2, RATTATA
-	dbw 3, HOPPIP
-	dbw 3, HOPPIP
+	dbw 2, FALINKS
+	dbw 2, FALINKS
+	dbw 3, FALINKS
+	dbw 3, FALINKS
+	dbw 2, FALINKS
+	dbw 3, FALINKS
+	dbw 3, FALINKS
 	; day
 	dbw 2, PIDGEY
 	dbw 2, SENTRET
