ac29735313f66ffa5b1d52860aa24a285d3702a3
Valencia-oh <valencia_oh@protonmail.com>
2025-12-07 10:28:40 -0500

    Update base_stats.asm

--- data/pokemon/base_stats.asm
+++ data/pokemon/base_stats.asm
@@ -31,9 +31,10 @@ ENDM
 
 BaseData::
 	indirect_table BASE_DATA_SIZE, 1
-	indirect_entries JOHTO_POKEMON - 1, BaseData1
-	indirect_entries FOREIGN_POKEMON - 1, BaseData2
-	indirect_entries NUM_POKEMON, BaseData3
+	indirect_entries KANTO_POKEMON - 1, BaseData1
+	indirect_entries JOHTO_POKEMON - 1, BaseData2
+	indirect_entries FOREIGN_POKEMON, BaseData3
+	indirect_entries NUM_POKEMON, BaseData4
 	indirect_table_end
 
 INCLUDE "data/pokemon/base_stats_kanto.asm"
