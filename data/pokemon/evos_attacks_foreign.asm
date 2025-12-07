fabd852b0f6b7f29a5719f09260a654d3d692df1
Valencia-oh <valencia_oh@protonmail.com>
2025-12-06 21:34:53 -0500

    Create evos_attacks_foreign.asm

---
+++ data/pokemon/evos_attacks_foreign.asm
@@ -0,0 +1,21 @@
+SECTION "Evolutions and Attacks 3", ROMX
+
+EvosAttacksPointers3::
+	dw CelebiEvosAttacks
+.IndirectEnd::
+
+FalinksEvosAttacks:
+	db 0 ; no more evolutions
+	dbw 1, TACKLE
+	dbw 1, PROTECT
+	dbw 5, ROCK_SMASH
+	dbw 10, FOCUS_ENERGY
+	dbw 15, HEADBUTT
+  dbw 20, METAL_CLAW
+	dbw 25, ENDURE
+	dbw 30, COUNTER
+	dbw 40, MEGA_HORN
+	dbw 50, COUNTER
+	db 0 ; no more level-up moves
+
+ENDSECTION
