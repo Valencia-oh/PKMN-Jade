cc2cbae7e0b782e8a7894c48271ce8454516048e
Valencia-oh <valencia_oh@protonmail.com>
2025-12-06 21:22:45 -0500

    Create falinks.asm

---
+++ data/pokemon/base_stats/falinks.asm
@@ -0,0 +1,21 @@
++	db FALINKS ; 252
++
++	db 65,  100,  100,   60,  70,  75
++	;   hp  atk  def  spd  sat  sdf
++
++	db FIGHTING, STEEL ; type
++	db 45 ; catch rate
++	db 165 ; base exp
++	db LEFTOVERS, LEFTOVERS ; items
++	db GENDER_UNKNOWN ; gender ratio
++	db 100 ; unknown 1
++	db 25 ; step cycles to hatch
++	db 5 ; unknown 2
++	INCBIN "gfx/pokemon/munchlax/front.dimensions"
++	dw NULL, NULL ; unused (beta front/back pics)
++	db GROWTH_MEDIUM_FAST ; growth rate
++	dn EGG_MINERAL, EGG_FAIRY ; egg groups
++
++	; tm/hm learnset
++	tmhm HYPER_BEAM, REST
++	; end
