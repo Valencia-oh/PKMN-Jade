e4ea2afd0afbf1fec7a8dd6e79ab1a708a60cb40
Valencia-oh <valencia_oh@protonmail.com>
2025-12-07 18:42:48 -0500

    Update icons.asm

--- gfx/icons.asm
+++ gfx/icons.asm
@@ -129,10 +129,6 @@ ElectabuzzIcon:: INCBIN "gfx/icons/electabuzz.2bpp"
 MagmarIcon::     INCBIN "gfx/icons/magmar.2bpp"
 PinsirIcon::     INCBIN "gfx/icons/pinsir.2bpp"
 TaurosIcon::     INCBIN "gfx/icons/tauros.2bpp"
-
-
-SECTION "Mon Icons 2", ROMX
-
 MagikarpIcon::   INCBIN "gfx/icons/magikarp.2bpp"
 GyaradosIcon::   INCBIN "gfx/icons/gyarados.2bpp"
 LaprasIcon::     INCBIN "gfx/icons/lapras.2bpp"
@@ -141,6 +137,9 @@ EeveeIcon::      INCBIN "gfx/icons/eevee.2bpp"
 VaporeonIcon::   INCBIN "gfx/icons/vaporeon.2bpp"
 JolteonIcon::    INCBIN "gfx/icons/jolteon.2bpp"
 FlareonIcon::    INCBIN "gfx/icons/flareon.2bpp"
+
+SECTION "Mon Icons 2", ROMX
+
 PorygonIcon::    INCBIN "gfx/icons/porygon.2bpp"
 OmanyteIcon::    INCBIN "gfx/icons/omanyte.2bpp"
 OmastarIcon::    INCBIN "gfx/icons/omastar.2bpp"
