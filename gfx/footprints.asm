011bb2b013350d7e5950ea3336c1868b9f307557
Valencia-oh <valencia_oh@protonmail.com>
2025-12-07 18:19:48 -0500

    Update footprints.asm

--- gfx/footprints.asm
+++ gfx/footprints.asm
@@ -254,6 +254,7 @@ INCBIN "gfx/footprints/ho_oh.1bpp"
 INCBIN "gfx/footprints/celebi.1bpp"
 INCBIN "gfx/footprints/applin.1bpp"
 INCBIN "gfx/footprints/appletun.1bpp"
+INCBIN "gfx/footprints/flapple.1bpp"
 INCBIN "gfx/footprints/falinks.1bpp"
 
 	assert_table_length NUM_POKEMON
