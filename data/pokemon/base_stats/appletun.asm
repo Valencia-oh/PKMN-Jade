5a39de3480979ea39bcb85adfdda101d10cccb94
Valencia-oh <valencia_oh@protonmail.com>
2025-12-07 16:32:49 -0500

    Create appletun.asm

---
+++ data/pokemon/base_stats/appletun.asm
@@ -0,0 +1,20 @@
+db APPLIN ; 252
+
+	db 110,  85,  80,   30,  80,  100
+	evs  2,   0,   0,   0,   0,   0
+	;   hp  atk  def  spd  sat  sdf
+
+	db GRASS, DRAGON ; type
+	db 45 ; catch rate
+	db 170 ; base exp
+	dw NO_ITEM, NO_ITEM ; items
+	db GENDER_F50 ; gender ratio
+	db 20 ; step cycles to hatch
+	INCBIN "gfx/pokemon/appletun/front.dimensions"
+	dw NULL, NULL ; unused (beta front/back pics)
+	db GROWTH_MEDIUM_SLOW ; growth rate
+	dn EGG_PLANT, EGG_DRAGON ; egg groups
+
+	; tm/hm learnset
+	tmhm TAKEDOWN, PROTECT, ATTRACT, RAIN_DANCE, SUNNY_DAY,
+	; end
