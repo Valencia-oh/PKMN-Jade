1d872718d34cd629a98df63dc0da7c2149bc733f
Valencia-oh <valencia_oh@protonmail.com>
2025-12-07 18:00:16 -0500

    Update anim_pointers.asm

--- gfx/pokemon/anim_pointers.asm
+++ gfx/pokemon/anim_pointers.asm
@@ -253,6 +253,6 @@ AnimationPointers:
 	dw CelebiAnimation
 	dw ApplinAnimation
 	dw AppletunAnimation
-	dw FlappletunAnimation
+	dw FlappleAnimation
 	dw FalinksAnimation
 	assert_table_length NUM_POKEMON
