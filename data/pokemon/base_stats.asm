24199cefff37a97b4f9577c08f906f4a7d9fdf37
Valencia-oh <valencia_oh@protonmail.com>
2025-12-07 18:05:07 -0500

    Update base_stats.asm

--- data/pokemon/base_stats.asm
+++ data/pokemon/base_stats.asm
@@ -288,5 +288,6 @@ INCLUDE "data/pokemon/base_stats/ho_oh.asm"
 INCLUDE "data/pokemon/base_stats/celebi.asm"
 INCLUDE "data/pokemon/base_stats/applin.asm"
 INCLUDE "data/pokemon/base_stats/appletun.asm"
+INCLUDE "data/pokemon/base_stats/flapple.asm"
 INCLUDE "data/pokemon/base_stats/falinks.asm"
 .IndirectEnd::
