1599a19ae0426a88da6b28257b8faa4eadb5123c
Valencia-oh <valencia_oh@protonmail.com>
2025-12-07 12:44:49 -0500

    Create applin.asm

---
+++ data/pokemon/dex_entries/applin.asm
@@ -0,0 +1,9 @@
+db "APPLE_CORE@" ; species name
+	dw 008, 0011 ; height, weight
+
+	db   "It spends its life"
+	next "inside an apple."
+	next "It hides from its "
+
+	page "natural enemies"
+	next "bird MON.@"
