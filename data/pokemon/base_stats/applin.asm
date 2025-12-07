1b3b82bcf2e5a25949a5336cc848553922f0980d
Valencia-oh <valencia_oh@protonmail.com>
2025-12-07 16:33:38 -0500

    Update applin.asm

--- data/pokemon/base_stats/applin.asm
+++ data/pokemon/base_stats/applin.asm
@@ -10,7 +10,7 @@ db APPLIN ; 252
 	dw NO_ITEM, NO_ITEM ; items
 	db GENDER_F50 ; gender ratio
 	db 25 ; step cycles to hatch
-	INCBIN "gfx/pokemon/falinks/front.dimensions"
+	INCBIN "gfx/pokemon/applin/front.dimensions"
 	dw NULL, NULL ; unused (beta front/back pics)
 	db GROWTH_MEDIUM_FAST ; growth rate
 	dn EGG_PLANT, EGG_DRAGON ; egg groups
