0752604f011b1d6ce050d8392e2acffe3140bfc0
Valencia-oh <valencia_oh@protonmail.com>
2025-12-07 12:50:22 -0500

    Update cries.asm

--- data/pokemon/cries.asm
+++ data/pokemon/cries.asm
@@ -258,4 +258,5 @@ PokemonCries::
 	mon_cry CRY_AIPOM,         0,  384 ; HO_OH
 	mon_cry CRY_ENTEI,       330,  273 ; CELEBI
 	mon_cry CRY_RHYDON,      -34,  124 ; FALINKS
+	mon_cry CRY_WOOPER,       43,   64 ; APPLIN
 	assert_table_length NUM_POKEMON
