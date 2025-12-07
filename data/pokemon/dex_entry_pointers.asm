2211ceb0f3491480da65635aab3ea8a675cf1ee5
Valencia-oh <valencia_oh@protonmail.com>
2025-12-07 12:55:57 -0500

    Update dex_entry_pointers.asm

--- data/pokemon/dex_entry_pointers.asm
+++ data/pokemon/dex_entry_pointers.asm
@@ -252,5 +252,6 @@ PokedexDataPointerTable:
 	dba LugiaPokedexEntry
 	dba HoOhPokedexEntry
 	dba CelebiPokedexEntry
+	dba ApplinPokedexEntry
 	dba FalinksPokedexEntry
 	assert_table_length NUM_POKEMON
