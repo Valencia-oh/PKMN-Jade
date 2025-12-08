2c2923e98745faa0cafbfa0c8a53a3786522cd5b
Valencia-oh <valencia_oh@protonmail.com>
2025-12-08 10:43:25 -0500

    Update idle_pointers.asm

--- gfx/pokemon/idle_pointers.asm
+++ gfx/pokemon/idle_pointers.asm
@@ -253,6 +253,7 @@ AnimationIdlePointers:
 	dw CelebiAnimationIdle
 	dw WingullAnimationIdle
 	dw PelipperAnimationIdle
+	dw TogekissAnimationIdle
 	dw MamoswineAnimationIdle
 	dw ApplinAnimationIdle
 	dw AppletunAnimationIdle
