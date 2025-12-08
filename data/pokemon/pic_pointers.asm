484407132cb519dec88c8897f0bbb0faf73da550
Valencia-oh <valencia_oh@protonmail.com>
2025-12-08 09:44:14 -0500

    Update pic_pointers.asm

--- data/pokemon/pic_pointers.asm
+++ data/pokemon/pic_pointers.asm
@@ -522,6 +522,8 @@ PokemonPicPointers::
 	dba WingullBackpic
 	dba PelipperFrontpic
 	dba PelipperBackpic
+	dba MamoswineFrontpic
+	dba MamoswineBackpic
 	dba ApplinFrontpic
 	dba ApplinBackpic
 	dba AppletunFrontpic
