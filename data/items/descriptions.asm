76c1d70fd610e53804b159f9dc1f84cc07182c02
Valencia-oh <valencia_oh@protonmail.com>
2025-12-08 11:17:18 -0500

    Update descriptions.asm

--- data/items/descriptions.asm
+++ data/items/descriptions.asm
@@ -82,7 +82,7 @@ ItemDescriptions1:
 	dw BigMushroomDesc  ; 0047
 	dw SilverPowderDesc ; 0048
 	dw BluApricornDesc  ; 0049
-	dw QuestionMarkDesc ; 004A
+	dw TartAppleDesc    ; 004A
 	dw AmuletCoinDesc   ; 004B
 	dw YlwApricornDesc  ; 004C
 	dw GrnApricornDesc  ; 004D
