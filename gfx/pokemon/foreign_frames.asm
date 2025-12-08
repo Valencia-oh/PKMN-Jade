e46c103bd1a8846c452dd82f519d33f95283d496
Valencia-oh <valencia_oh@protonmail.com>
2025-12-08 18:00:31 -0500

    Update foreign_frames.asm

--- gfx/pokemon/foreign_frames.asm
+++ gfx/pokemon/foreign_frames.asm
@@ -1,5 +1,6 @@
 WingullFrames:   INCLUDE "gfx/pokemon/wingull/frames.asm"
 PelipperFrames: INCLUDE "gfx/pokemon/pelipper/frames.asm"
+HonchkrowFrames: INCLUDE "gfx/pokemon/honchkrow/frames.asm"
 TogekissFrames: INCLUDE "gfx/pokemon/togekiss/frames.asm"
 MamoswineFrames: INCLUDE "gfx/pokemon/mamoswine/frames.asm"
 ApplinFrames:   INCLUDE "gfx/pokemon/applin/frames.asm"
