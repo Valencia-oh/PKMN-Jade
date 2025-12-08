fbf7c4661cdfa9dec81375861c008733cda2f692
Valencia-oh <valencia_oh@protonmail.com>
2025-12-07 20:16:20 -0500

    Create pelipper.asm

---
+++ data/pokemon/base_stats/pelipper.asm
@@ -0,0 +1,20 @@
+db PELIPPER ; 258
+
+	db 60,  50,  100,   65,  85,  70
+	evs  0,   0,   2,   0,   0,   0
+	;   hp  atk  def  spd  sat  sdf
+
+	db FLYING, WATER ; type
+	db 45 ; catch rate
+	db 164 ; base exp
+	dw NO_ITEM, NO_ITEM ; items
+	db GENDER_F50 ; gender ratio
+	db 20 ; step cycles to hatch
+	INCBIN "gfx/pokemon/wingull/front.dimensions"
+	dw NULL, NULL ; unused (beta front/back pics)
+	db GROWTH_MEDIUM_FAST ; growth rate
+	dn EGG_WATER, EGG_FLYING ; egg groups
+
+	; tm/hm learnset
+	tmhm PROTECT, ATTRACT, RAIN_DANCE, SUNNY_DAY, HIDDEN_POWER
+	; end
