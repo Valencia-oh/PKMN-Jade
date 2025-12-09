48790bf85a56986023bb8caf283bd4923dcd04f2
Valencia-oh <valencia_oh@protonmail.com>
2025-12-09 17:02:31 -0500

    Revert "Update conversion2.asm"
    This reverts commit 8f19541b6fbee7b35100e6a3e424252581c31a5b.

--- engine/battle/move_effects/conversion2.asm
+++ engine/battle/move_effects/conversion2.asm
@@ -16,7 +16,6 @@ BattleCommand_Conversion2:
 	ld l, a
 	ld a, MOVE_TYPE
 	call GetMoveAttribute
-	and TYPE_MASK
 	ld d, a
 	pop hl
 	cp CURSE_TYPE
@@ -44,7 +43,6 @@ BattleCommand_Conversion2:
 	ld a, d
 	ld [hl], a
 	call BattleCheckTypeMatchup
-	and TYPE_MASK
 	pop hl
 	pop af
 	ld [hl], a
