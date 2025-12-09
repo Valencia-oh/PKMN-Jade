7e06b46e83d02d11c7f39bafc5a8b38ba668db77
Valencia-oh <valencia_oh@protonmail.com>
2025-12-09 17:03:57 -0500

    Revert "Update core.asm"
    This reverts commit 6d824a9e772883ab5e159e15c7f3268140e8ad76.

--- engine/battle/core.asm
+++ engine/battle/core.asm
@@ -5506,17 +5506,14 @@ MoveInfoBox:
 	ld [wStringBuffer1], a
 	call .PrintPP
 
-farcall UpdateMoveData
-	ld a, [wPlayerMoveStruct + MOVE_ANIM]
-	ld b, a
-	farcall GetMoveCategoryName
 	hlcoord 1, 9
-	ld de, wStringBuffer1
+	ld de, .Type
 	rst PlaceString
 
-	ld h, b
-	ld l, c
+	hlcoord 7, 11
+	ld [hl], '/'
 
+	farcall UpdateMoveData
 	ld a, [wPlayerMoveStruct + MOVE_ANIM]
 	ld b, a
 	hlcoord 2, 10
@@ -5524,7 +5521,8 @@ farcall UpdateMoveData
 
 .Disabled:
 	db "Disabled!@"
-
+.Type:
+	db "TYPE/@"
 
 .PrintPP:
 	hlcoord 5, 11
