af4e1bfb18aa8b7ee5488b87e1de12a71f1b4d6d
Valencia-oh <valencia_oh@protonmail.com>
2025-12-09 10:22:01 -0500

    Create ersrheadbby.asm

---
+++ data/pokemon/base_stats/ersrheadbby.asm
@@ -0,0 +1,20 @@
+	db 0 ; species ID placeholder
+
+	db  40,  45,  30, 90, 150,  35
+	evs  0,   0,   0,   0,   1,   0
+	;   hp  atk  def  spd  sat  sdf
+
+	db PSYCHIC_TYPE, DARK_TYPE ; type
+	db 200 ; catch rate
+	db 45 ; base exp
+	dw NO_ITEM, NO_ITEM ; items
+	db GENDER_F50 ; gender ratio
+	db 20 ; step cycles to hatch
+	INCBIN "gfx/pokemon/ersrheadbby/front.dimensions"
+	dw NULL, NULL ; unused (beta front/back pics)
+	db GROWTH_FAST ; growth rate
+	dn EGG_HUMANSHAPE, EGG_HUMANSHAPE ; egg groups
+
+	; tm/hm learnset
+	tmhm DYNAMICPUNCH, HEADBUTT, CURSE, TOXIC, ZAP_CANNON, PSYCH_UP, HIDDEN_POWER, SUNNY_DAY, SNORE, PROTECT, RAIN_DANCE, ENDURE, FRUSTRATION, RETURN, DIG, PSYCHIC_M, SHADOW_BALL, DOUBLE_TEAM, ICE_PUNCH, SWAGGER, SLEEP_TALK, THUNDERPUNCH, DREAM_EATER, REST, ATTRACT, THIEF, FIRE_PUNCH, NIGHTMARE, FLASH
+	; end
