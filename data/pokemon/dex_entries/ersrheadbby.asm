24eb1555b98bedae6a65f253910420a0b3c85291
Valencia-oh <valencia_oh@protonmail.com>
2025-12-09 10:20:11 -0500

    Create ersrheadbby.asm

---
+++ data/pokemon/dex_entries/ersrheadbby.asm
@@ -0,0 +1,10 @@
+	db "SUFFERING@" ; species name
+	dw 010, 002 ; height, weight
+
+	db   "All it does is,"
+	next "cry and be a"
+	next "hard to get"
+
+	page "metaphor for"
+	next "parenthood"
+	next "or something.@"
