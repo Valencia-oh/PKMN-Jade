4ee28928058bc8eccab9e9ec967b4d29b4de0318
Valencia-oh <valencia_oh@protonmail.com>
2025-12-10 10:43:33 -0500

    Create larvesta.asm

---
+++ data/pokemon/dex_entries/larvesta.asm
@@ -0,0 +1,10 @@
+	db "TORCH@" ; species name
+	dw 307, 0635 ; height, weight
+
+	db   "This Pokémon was,"
+	next "believed to have been"
+	next "born from the sun"
+
+	page "When it evolves,"
+	next "its entire body is"
+	next "covered in fire.@"
