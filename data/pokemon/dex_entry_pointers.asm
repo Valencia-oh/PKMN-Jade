692d3856da3bdd925dd6edc4c6cf7556f02c766c
Valencia-oh <valencia_oh@protonmail.com>
2025-12-10 10:10:17 -0500

    Update dex_entry_pointers.asm

--- data/pokemon/dex_entry_pointers.asm
+++ data/pokemon/dex_entry_pointers.asm
@@ -257,6 +257,7 @@ PokedexDataPointerTable:
 	dba HonchkrowPokedexEntry
 	dba TogekissPokedexEntry
 	dba MamoswinePokedexEntry
+	dba VolcaronaPokedexEntry
 	dba ApplinPokedexEntry
 	dba AppletunPokedexEntry
 	dba FlapplePokedexEntry
