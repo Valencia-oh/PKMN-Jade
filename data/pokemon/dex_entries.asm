97c9898ccbc1ff875012fda5a2ff39399312c99f
Valencia-oh <valencia_oh@protonmail.com>
2025-12-10 10:40:34 -0500

    Update dex_entries.asm

--- data/pokemon/dex_entries.asm
+++ data/pokemon/dex_entries.asm
@@ -270,6 +270,7 @@ TogekissPokedexEntry::   INCLUDE "data/pokemon/dex_entries/togekiss.asm"
 
 SECTION "Pokedex Entries 257-320", ROMX
 MamoswinePokedexEntry::  INCLUDE "data/pokemon/dex_entries/mamoswine.asm"
+LarvestaPokedexEntry::  INCLUDE "data/pokemon/dex_entries/larvesta.asm"
 VolcaronaPokedexEntry::  INCLUDE "data/pokemon/dex_entries/volcarona.asm"
 ApplinPokedexEntry::     INCLUDE "data/pokemon/dex_entries/applin.asm"
 AppletunPokedexEntry::   INCLUDE "data/pokemon/dex_entries/appletun.asm"
