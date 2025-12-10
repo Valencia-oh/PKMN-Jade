88e9310e98b83ff890cde9ec4e7f0a7527ab60b8
Valencia-oh <valencia_oh@protonmail.com>
2025-12-10 10:13:54 -0500

    Create volcarona.asm

---
+++ data/pokemon/dex_entries/volcarona.asm
@@ -0,0 +1,10 @@
+	db "SUN@" ; species name
+	dw 503, 1014 ; height, weight
+
+	db   "When volcanic ash,"
+	next "darkened the atmosphere"
+	next "it is said that"
+
+	page "Volcarona's fire"
+	next "was a replacement"
+	next "for the sun.@"
