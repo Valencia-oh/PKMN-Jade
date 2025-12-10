70c09d91fb6dcba3a34c72c695731b5dd46dceaf
Valencia-oh <valencia_oh@protonmail.com>
2025-12-10 10:16:34 -0500

    Create volcarona.asm

---
+++ data/pokemon/base_stats/volcarona.asm
@@ -0,0 +1,20 @@
+	db 0 ; species ID placeholder
+
+	db  85,  60, 65,  100,  135,  105
+	evs  0,   0,   0,   0,   3,   0
+	;   hp  atk  def  spd  sat  sdf
+
+	db FIRE, BUG ; type
+	db 15 ; catch rate
+	db 248 ; base exp
+	dw NO_ITEM, NO_ITEM ; items
+	db GENDER_F50 ; gender ratio
+	db 40 ; step cycles to hatch
+	INCBIN "gfx/pokemon/volcarona/front.dimensions"
+	dw NULL, NULL ; unused (beta front/back pics)
+	db GROWTH_SLOW ; growth rate
+	dn EGG_BUG, EGG_BUG ; egg groups
+
+	; tm/hm learnset
+	tmhm CURSE, ROLLOUT, TOXIC, ROCK_SMASH, HIDDEN_POWER, SUNNY_DAY, SNORE, HYPER_BEAM, PROTECT, ENDURE, FRUSTRATION, EARTHQUAKE, RETURN, MUD_SLAP, DOUBLE_TEAM, SWAGGER, SLEEP_TALK, FIRE_BLAST, DEFENSE_CURL, REST, ATTRACT, STRENGTH, FLAMETHROWER
+	; end
