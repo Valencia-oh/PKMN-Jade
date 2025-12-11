c9813ece109f93ad7dec78e4d00dde6b5d498915
Valencia-oh <valencia_oh@protonmail.com>
2025-12-10 20:54:36 -0500

    Create galvantula.asm

---
+++ data/pokemon/dex_entries/galvantula.asm
@@ -0,0 +1,10 @@
+	db "ELESPIDER@" ; species name
+	dw 207, 0315 ; height, weight
+
+	db   "When attacked"
+	next "they create an"
+	next "electric barrier"
+
+	page "for sucking"
+	next "by spitting out many"
+	next "electrical threads.@"
