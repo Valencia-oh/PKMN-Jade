99b0edbfd64af18070ab7227559f599dafe4ae73
Valencia-oh <valencia_oh@protonmail.com>
2025-12-11 10:53:22 -0500

    Create electrike.asm

---
+++ data/pokemon/base_stats/electrike.asm
@@ -0,0 +1,20 @@
+	db 0 ; species ID placeholder
+
+	db  40,  45,  40,  65,  65,  40
+	evs  0,   0,   0,   1,   0,   0
+	;   hp  atk  def  spd  sat  sdf
+
+	db ELECTRIC, ELECTRIC ; type
+	db 120 ; catch rate
+	db 104 ; base exp
+	dw NO_ITEM, BERRY ; items
+	db GENDER_F50 ; gender ratio
+	db 20 ; step cycles to hatch
+	INCBIN "gfx/pokemon/manectric/front.dimensions"
+	dw NULL, NULL ; unused (beta front/back pics)
+	db GROWTH_SLOW ; growth rate
+	dn EGG_GROUND, EGG_GROUND ; egg groups
+
+	; tm/hm learnset
+	tmhm HEADBUTT, CURSE, ROLLOUT, TOXIC, ZAP_CANNON, HIDDEN_POWER, SNORE, PROTECT, RAIN_DANCE, ENDURE, FRUSTRATION, IRON_TAIL, THUNDER, RETURN, MUD_SLAP, DOUBLE_TEAM, SWAGGER, SLEEP_TALK, SWIFT, DEFENSE_CURL, DETECT, REST, ATTRACT, STRENGTH, FLASH, THUNDERBOLT
+	; end
