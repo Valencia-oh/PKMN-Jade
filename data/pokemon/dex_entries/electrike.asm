82b5e231b8930e9439a0d43d9f740e34ac012098
Valencia-oh <valencia_oh@protonmail.com>
2025-12-11 10:47:24 -0500

    Create electrike.asm

---
+++ data/pokemon/dex_entries/electrike.asm
@@ -0,0 +1,10 @@
+	db "LIGHTNING@" ; species name
+	dw 200, 335 ; height, weight
+
+	db   "It stores static"
+	next "electricity inits"
+	next "fur for discharging"
+
+	page "It gives off "
+	next "sparks if a storm "
+	next "approaches.@"
