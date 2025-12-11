fdaec1d86a9e27b7cb637a44e4b408c13f478e70
Valencia-oh <valencia_oh@protonmail.com>
2025-12-11 09:29:03 -0500

    Create mabosstiff.asm

---
+++ data/pokemon/dex_entries/mabosstiff.asm
@@ -0,0 +1,10 @@
+	db "BOSS@" ; species name
+	dw 307, 1345 ; height, weight
+
+	db   "Mabosstiff loves"
+	next "playing with kids"
+	next "Though usually"
+
+	page "gentle, it takes on"
+	next "an intimidating look"
+	next "for its family.@"
