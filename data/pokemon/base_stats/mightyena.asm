96a0d03ecf501af5d5abc0edb8d712e48d7344b9
Valencia-oh <valencia_oh@protonmail.com>
2025-12-11 13:40:24 -0500

    Create mightyena.asm

---
+++ data/pokemon/base_stats/mightyena.asm
@@ -0,0 +1,20 @@
+	db 0 ; species ID placeholder
+
+	db  70,  90,  70,  70,  60,  60
+	evs  0,   2,   0,   0,   0,   0
+	;   hp  atk  def  spd  sat  sdf
+
+	db DARK, DARK ; type
+	db 127 ; catch rate
+	db 128 ; base exp
+	dw NO_ITEM, NO_ITEM ; items
+	db GENDER_F50 ; gender ratio
+	db 15 ; step cycles to hatch
+	INCBIN "gfx/pokemon/maschiff/front.dimensions"
+	dw NULL, NULL ; unused (beta front/back pics)
+	db GROWTH_MEDIUM_SLOW ; growth rate
+	dn EGG_GROUND, EGG_GROUND ; egg groups
+
+	; tm/hm learnset
+	tmhm HEADBUTT, CURSE, ROAR, TOXIC, ROCK_SMASH, HIDDEN_POWER, SNORE, PROTECT, ENDURE, FRUSTRATION, IRON_TAIL, RETURN, SHADOW_BALL, MUD_SLAP, DOUBLE_TEAM, SWAGGER, SLEEP_TALK, SLUDGE_BOMB, FIRE_BLAST, SWIFT, DREAM_EATER, DETECT, REST, ATTRACT, THIEF, NIGHTMARE
+	; end
