0d225eb4667a887e37e8a46145f659c8c7ba56ed
Valencia-oh <valencia_oh@protonmail.com>
2025-12-11 13:24:39 -0500

    Update icons.asm

--- gfx/icons.asm
+++ gfx/icons.asm
@@ -258,6 +258,8 @@ CelebiIcon::     INCBIN "gfx/icons/celebi.2bpp"
 
 SECTION "Mon Icons 3", ROMX
 
+PoochyenaIcon::  INCBIN "gfx/icons/houndour.2bpp"
+MightyenaIcon::  INCBIN "gfx/icons/houndoom.2bpp"
 ElectrikeIcon::  INCBIN "gfx/icons/houndour.2bpp"
 ManectricIcon::  INCBIN "gfx/icons/houndoom.2bpp"
 WingullIcon::    INCBIN "gfx/icons/skarmory.2bpp"
