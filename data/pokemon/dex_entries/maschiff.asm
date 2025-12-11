7f7ba48d414424da2abca7fd638af53f21477e25
Valencia-oh <valencia_oh@protonmail.com>
2025-12-11 09:31:27 -0500

    Create maschiff.asm

---
+++ data/pokemon/dex_entries/maschiff.asm
@@ -0,0 +1,10 @@
+	db "RASCAL@" ; species name
+	dw 108, 353 ; height, weight
+
+	db   "Its well-developed"
+	next "jaw and fangs are"
+	next "strong enough to"
+
+	page "crunch through rocks"
+	next "and its thick fat"
+	next "is a great defense.@"
