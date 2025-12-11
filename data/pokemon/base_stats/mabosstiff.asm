53f5dcb3ea337b1be4bd9a16c74f1e607aa221ff
Valencia-oh <valencia_oh@protonmail.com>
2025-12-11 09:26:56 -0500

    Create mabosstiff.asm

---
+++ data/pokemon/base_stats/mabosstiff.asm
@@ -0,0 +1,20 @@
+	db 0 ; species ID placeholder
+
+	db  80,  120,  90,  85,  60,  70
+	evs  0,   2,   0,   0,   0,   0
+	;   hp  atk  def  spd  sat  sdf
+
+	db DARK, DARK ; type
+	db 75 ; catch rate
+	db 177 ; base exp
+	dw NO_ITEM, NO_ITEM ; items
+	db GENDER_F50 ; gender ratio
+	db 20 ; step cycles to hatch
+	INCBIN "gfx/pokemon/mabosstiff/front.dimensions"
+	dw NULL, NULL ; unused (beta front/back pics)
+	db GROWTH_MEDIUM_SLOW ; growth rate
+	dn EGG_FIELD, EGG_FIELD ; egg groups
+
+	; tm/hm learnset
+	tmhm HEADBUTT, CURSE, ROAR, TOXIC, ROCK_SMASH, HIDDEN_POWER, SNORE, PROTECT, ENDURE, FRUSTRATION, IRON_TAIL, RETURN, SHADOW_BALL, MUD_SLAP, DOUBLE_TEAM, SWAGGER, SLEEP_TALK, SLUDGE_BOMB, FIRE_BLAST, SWIFT, DREAM_EATER, DETECT, REST, ATTRACT, THIEF, NIGHTMARE
+	; end
