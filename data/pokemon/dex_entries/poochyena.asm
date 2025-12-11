3daf629fcd4cdd4566b7c509a9f8499e05a574c4
Valencia-oh <valencia_oh@protonmail.com>
2025-12-11 13:48:04 -0500

    Create poochyena.asm

---
+++ data/pokemon/dex_entries/poochyena.asm
@@ -0,0 +1,10 @@
+	db "BITE@" ; species name
+	dw 108, 300 ; height, weight
+
+	db   "It has a very "
+	next "tenacious nature."
+	next "Further it has an"
+
+	page "acute sense of smell"
+	next "lets it chase prey"
+	next "without ever losing it.@"
