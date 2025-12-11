835a7e3fc96e5b0d4cb4cce8fdf3e19a5541dbd5
Valencia-oh <valencia_oh@protonmail.com>
2025-12-10 20:47:27 -0500

    Create joltik.asm

---
+++ data/pokemon/dex_entries/joltik.asm
@@ -0,0 +1,10 @@
+	db "ATTACHING@" ; species name
+	dw 004, 0013 ; height, weight
+
+	db   "Joltik that live"
+	next "in cities have "
+	next "learned a techique "
+
+	page "for sucking"
+	next "electricity from the"
+	next "outlets in houses.@"
