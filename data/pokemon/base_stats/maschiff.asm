10f7dd2d5179d20845ce395518116ffc9f42bbc2
Valencia-oh <valencia_oh@protonmail.com>
2025-12-11 09:25:28 -0500

    Create maschiff.asm

---
+++ data/pokemon/base_stats/maschiff.asm
@@ -0,0 +1,20 @@
+	db 0 ; species ID placeholder
+
+	db  60,  78,  60,  51,  40,  51
+	evs  0,   1,   0,   0,   0,   0
+	;   hp  atk  def  spd  sat  sdf
+
+	db DARK, DARK ; type
+	db 150 ; catch rate
+	db 114 ; base exp
+	dw NO_ITEM, NO_ITEM ; items
+	db GENDER_F50 ; gender ratio
+	db 20 ; step cycles to hatch
+	INCBIN "gfx/pokemon/maschiff/front.dimensions"
+	dw NULL, NULL ; unused (beta front/back pics)
+	db GROWTH_MEDIUM_SLOW ; growth rate
+	dn EGG_FIELD, EGG_FIELD ; egg groups
+
+	; tm/hm learnset
+	tmhm HEADBUTT, CURSE, ROAR, TOXIC, ROCK_SMASH, HIDDEN_POWER, SNORE, PROTECT, ENDURE, FRUSTRATION, IRON_TAIL, RETURN, SHADOW_BALL, MUD_SLAP, DOUBLE_TEAM, SWAGGER, SLEEP_TALK, SLUDGE_BOMB, FIRE_BLAST, SWIFT, DREAM_EATER, DETECT, REST, ATTRACT, THIEF, NIGHTMARE
+	; end
