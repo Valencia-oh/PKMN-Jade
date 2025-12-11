9dc45a44caef40ac64be1c052d67009876d51dfb
Valencia-oh <valencia_oh@protonmail.com>
2025-12-11 10:49:30 -0500

    Create manectric.asm

---
+++ data/pokemon/dex_entries/manectric.asm
@@ -0,0 +1,10 @@
+	db "DISCHARGE@" ; species name
+	dw 411, 886 ; height, weight
+
+	db   "Because lightning"
+	next "falls in their"
+	next "vicinities, Manectric"
+
+	page "were thought to"
+	next "have been born"
+	next "from lightning.@"
