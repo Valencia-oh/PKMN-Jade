5b53b5553a87dd3cb362446d15c268d7a1f74b6a
Valencia-oh <valencia_oh@protonmail.com>
2025-12-11 16:01:33 -0500

    Create shroomish.asm

---
+++ data/pokemon/dex_entries/shroomish.asm
@@ -0,0 +1,10 @@
+	db "MUSHROOM@" ; species name
+	dw 104, 99 ; height, weight
+
+	db   "It prefers damp "
+	next "places. By day it"
+	next "remains still in the "
+
+	page "forest shade. It "
+	next "releases toxic powder"
+	next "from its head.@"
