71dec95b7ca15bafcf3757086808e949e01a0516
Valencia-oh <valencia_oh@protonmail.com>
2025-12-11 13:38:43 -0500

    Create mightyena.asm

---
+++ data/pokemon/dex_entries/mightyena.asm
@@ -0,0 +1,10 @@
+	db "BITE@" ; species name
+	dw 303, 816 ; height, weight
+
+	db   "It will always"
+	next "obey the commands"
+	next "of a skilled Trainer"
+
+	page "Its behavior arises"
+	next "from its living in "
+	next "packs long ago.@"
