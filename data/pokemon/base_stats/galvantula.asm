900bbf7f1b01c68dd4811a8d00e552f856c1c184
Valencia-oh <valencia_oh@protonmail.com>
2025-12-10 20:52:17 -0500

    Create galvantula.asm

---
+++ data/pokemon/base_stats/galvantula.asm
@@ -0,0 +1,20 @@
+	db 0 ; species ID placeholder
+
+	db  70,  77,  60,  108,  97,  60
+	evs  0,   0,   0,   2,   0,   0
+	;   hp  atk  def  spd  sat  sdf
+
+	db BUG, ELECTRIC ; type
+	db 75 ; catch rate
+	db 165 ; base exp
+	dw NO_ITEM, NO_ITEM ; items
+	db GENDER_F50 ; gender ratio
+	db 20 ; step cycles to hatch
+	INCBIN "gfx/pokemon/galvantula/front.dimensions"
+	dw NULL, NULL ; unused (beta front/back pics)
+	db GROWTH_MEDIUM_FAST ; growth rate
+	dn EGG_BUG, EGG_BUG ; egg groups
+
+	; tm/hm learnset
+	tmhm CURSE, TOXIC, HIDDEN_POWER, SUNNY_DAY, SNORE, HYPER_BEAM, PROTECT, GIGA_DRAIN, ENDURE, FRUSTRATION, SOLARBEAM, RETURN, DIG, PSYCHIC_M, THUNDER, RETURN, SHADOW_BALL, MUD_SLAP, DOUBLE_TEAM, SWAGGER, SLEEP_TALK, SWIFT, DETECT, REST, ATTRACT, FLASH, THUNDERBOLT
+	; end
