f16049e4aeb3c318822368d03db45d2918f1c08e
Valencia-oh <valencia_oh@protonmail.com>
2025-12-12 14:50:24 -0500

    Create flittle.asm

---
+++ data/pokemon/dex_entries/flittle.asm
@@ -0,0 +1,10 @@
+	db "FRILL@" ; species name
+	dw 211, 430 ; height, weight
+
+	db   "It spends its"
+	next "time running"
+	next "around wastelands"
+
+	page "looking for its"
+	next "favorite berries"
+	next "to eat.@"
