e5989fd849fcd63dbbdb551f662e9f672a009ff4
Valencia-oh <valencia_oh@protonmail.com>
2025-12-11 22:30:20 -0500

    Update redundant.asm

--- engine/battle/ai/redundant.asm
+++ engine/battle/ai/redundant.asm
@@ -43,6 +43,7 @@ AI_Redundant:
 	dbw EFFECT_SYNTHESIS,    .Synthesis
 	dbw EFFECT_MOONLIGHT,    .Moonlight
 	dbw EFFECT_SWAGGER,      .Swagger
+	dbw EFFECT_ROOST,        .Roost
 	dbw EFFECT_FUTURE_SIGHT, .FutureSight
 	db -1
 
@@ -184,8 +185,10 @@ AI_Redundant:
 .MorningSun:
 .Synthesis:
 .Moonlight:
+.Roost:
 	farcall AICheckEnemyMaxHP
 	jr nc, .NotRedundant
+
 
 .Teleport:
 .Redundant:
