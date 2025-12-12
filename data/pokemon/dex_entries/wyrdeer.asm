c070f74066bcc4765f85efd80c5862d8a30d0752
Valencia-oh <valencia_oh@protonmail.com>
2025-12-11 22:15:24 -0500

    Create wyrdeer.asm

---
+++ data/pokemon/dex_entries/wyrdeer.asm
@@ -0,0 +1,10 @@
+	db "BIG HORN@" ; species name
+	dw 511, 2097 ; height, weight
+
+	db   "once thought wiped,"
+	next "out due to hunting"
+	next "for their horns"
+
+	page "this MON has"
+	next "begun to recover"
+	next "along mount silver.@"
