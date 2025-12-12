171438d14ccc0a7b7a703b74e2c2553ab982b5f3
Valencia-oh <valencia_oh@protonmail.com>
2025-12-11 22:39:36 -0500

    Update effect_command_pointers.asm

--- data/battle/effect_command_pointers.asm
+++ data/battle/effect_command_pointers.asm
@@ -180,5 +180,5 @@ BattleCommandPointers:
 	dw BattleCommand_SuperEffectiveLoopText
 	dw BattleCommand_StartLoop
 	dw BattleCommand_Curl
-	dw BattleCommand Roost
+	dw BattleCommand_Roost
 	assert_table_length NUM_EFFECT_COMMANDS
