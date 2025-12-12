3b7b006176a4e14976c0befcc4d622af8e7e49a7
Valencia-oh <valencia_oh@protonmail.com>
2025-12-11 22:45:20 -0500

    Update moves.asm

--- data/moves/moves.asm
+++ data/moves/moves.asm
@@ -280,7 +280,6 @@ MovesGen2:
 	move EFFECT_DEFENSE_DOWN_HIT,   20, FIGHTING,      100, 15,  50      ;ROCK_SMASH
 	move EFFECT_TRAP_TARGET,        15, WATER,          70, 15,   0      ;WHIRLPOOL
 	move EFFECT_BEAT_UP,            10, DARK,          100, 10,   0      ;BEAT_UP
-	move EFFECT_ROOST,               0, FLYING,          -1, 5,   0		 ;ROOST
 .IndirectEnd::
 
 POPS
