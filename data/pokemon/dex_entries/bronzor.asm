32de72df3df648f5b8f187cdad9974981f89d6e8
Valencia-oh <valencia_oh@protonmail.com>
2025-12-12 14:46:59 -0500

    Create bronzor.asm

---
+++ data/pokemon/dex_entries/bronzor.asm
@@ -0,0 +1,10 @@
+	db "BRONZE@" ; species name
+	dw 108, 1334 ; height, weight
+
+	db   "Ancient people"
+	next "believed that the"
+	next "pattern on Bronzor's"
+
+	page "back contained"
+	next "a mysterious"
+	next "power.@"
