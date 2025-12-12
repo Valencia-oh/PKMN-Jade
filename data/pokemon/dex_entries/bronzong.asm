42550b57da6b72c836239c1b2f41ec5717be30da
Valencia-oh <valencia_oh@protonmail.com>
2025-12-12 14:48:59 -0500

    Create bronzong.asm

---
+++ data/pokemon/dex_entries/bronzong.asm
@@ -0,0 +1,10 @@
+	db "BRONZE_BELL@" ; species name
+	dw 403, 4123 ; height, weight
+
+	db   "Ancient people"
+	next "believed that"
+	next "petitioning Bronzong"
+
+	page "for rain was the"
+	next "way to make"
+	next "crops grow.@"
