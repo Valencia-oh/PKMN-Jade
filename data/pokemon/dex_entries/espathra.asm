8c754dc3c94d61507b6ce58f3df121f5c0f7b158
Valencia-oh <valencia_oh@protonmail.com>
2025-12-12 14:52:33 -0500

    Create espathra.asm

---
+++ data/pokemon/dex_entries/espathra.asm
@@ -0,0 +1,10 @@
+	db "OSTRICH@" ; species name
+	dw 603, 1984 ; height, weight
+
+	db   "It emits psychic"
+	next "power from the aps "
+	next "between its colorful"
+
+	page "frills and sprints"
+	next "speeds greater"
+	next "than 120 MPH.@"
