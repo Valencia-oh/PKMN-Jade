0a96b4de3e02cd487fcfde5364713be48473fd06
Valencia-oh <valencia_oh@protonmail.com>
2025-12-13 16:23:58 -0500

    Create linoone.asm

---
+++ data/pokemon/base_stats/linoone.asm
@@ -0,0 +1,20 @@
+	db 0 ; species ID placeholder
+
+	db  78,  70,  71,  100,  50,  61
+	evs  0,   0,   0,   2,   0,   0
+	;   hp  atk  def  spd  sat  sdf
+
+	db NORMAL, GROUND ; type
+	db 90 ; catch rate
+	db 147 ; base exp
+	dw NO_ITEM, NO_ITEM ; items
+	db GENDER_F50 ; gender ratio
+	db 15 ; step cycles to hatch
+	INCBIN "gfx/pokemon/linoone/front.dimensions"
+	dw NULL, NULL ; unused (beta front/back pics)
+	db GROWTH_MEDIUM_FAST ; growth rate
+	dn EGG_GROUND, EGG_GROUND ; egg groups
+
+	; tm/hm learnset
+	tmhm CURSE, TOXIC, ROCK_SMASH, HIDDEN_POWER, SUNNY_DAY, SNORE, PROTECT, ENDURE, FRUSTRATION, EARTHQUAKE, RETURN, DIG, MUD_SLAP, DOUBLE_TEAM, SWAGGER, SLEEP_TALK, SLUDGE_BOMB, REST, ATTRACT, THIEF, CUT
+	; end
