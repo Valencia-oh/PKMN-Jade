f07ab01b5a76cbb2d75d95dc5289469a9d0e9986
Valencia-oh <valencia_oh@protonmail.com>
2025-12-12 19:56:21 -0500

    Create malamar.asm

---
+++ data/pokemon/base_stats/malamar.asm
@@ -0,0 +1,20 @@
+	db 0 ; species ID placeholder
+
+	db  86,  92,  88, 73,  68,  75
+	evs  0,   2,   0,   0,   0,   0
+	;   hp  atk  def  spd  sat  sdf
+
+	db DARK, PSYCHIC_TYPE ; type
+	db 80 ; catch rate
+	db 169 ; base exp
+	dw NO_ITEM, NO_ITEM ; items
+	db GENDER_F50 ; gender ratio
+	db 20 ; step cycles to hatch
+	INCBIN "gfx/pokemon/malamar/front.dimensions"
+	dw NULL, NULL ; unused (beta front/back pics)
+	db GROWTH_MEDIUM_FAST ; growth rate
+	dn EGG_WATER1, EGG_WATER2 ; egg groups
+
+	; tm/hm learnset
+	tmhm CURSE, TOXIC, HIDDEN_POWER, SUNNY_DAY, SNORE, HYPER_BEAM, PROTECT, GIGA_DRAIN, ENDURE, FRUSTRATION, RETURN, DOUBLE_TEAM, SWAGGER, SLEEP_TALK, SWIFT, DETECT, REST, ATTRACT, THIEF, STRENGTH, PSYCHIC_M, THIEF, SLUDGE_BOMB, DREAM_EATER, NIGHTMARE
+	; end
