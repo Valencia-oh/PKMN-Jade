72437f927f3ec12c397457c1286e4c43cb3e67b6
Valencia-oh <valencia_oh@protonmail.com>
2025-12-12 20:01:55 -0500

    Create malamar.asm

---
+++ data/pokemon/dex_entries/malamar.asm
@@ -0,0 +1,10 @@
+	db "OVERTURNING@" ; species name
+	dw 211, 430 ; height, weight
+
+	db   "It wields the most"
+	next "compelling hypnotic"
+	next "powers of any MON"
+
+	page "and it forces"
+	next "others to do"
+	next "whatever it wants.@"
