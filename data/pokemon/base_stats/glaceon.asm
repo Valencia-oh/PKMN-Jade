0fc8ad29f1c2b3d69efecb78d66b8508bab92c0d
Valencia-oh <valencia_oh@protonmail.com>
2025-12-12 19:51:01 -0500

    Create glaceon.asm

---
+++ data/pokemon/base_stats/glaceon.asm
@@ -0,0 +1,20 @@
+	db 0 ; species ID placeholder
+
+	db  60,  65, 110,  65,  130, 95
+	evs  0,   0,   0,   2,   0,   0
+	;   hp  atk  def  spd  sat  sdf
+
+	db ICE, ICE ; type
+	db 45 ; catch rate
+	db 197 ; base exp
+	dw NO_ITEM, NO_ITEM ; items
+	db GENDER_F12_5 ; gender ratio
+	db 35 ; step cycles to hatch
+	INCBIN "gfx/pokemon/leafeon/front.dimensions"
+	dw NULL, NULL ; unused (beta front/back pics)
+	db GROWTH_MEDIUM_FAST ; growth rate
+	dn EGG_GROUND, EGG_GROUND ; egg groups
+
+	; tm/hm learnset
+	tmhm HEADBUTT, BLIZZARD, ICE_BEAM, GIGA_DRAIN, CURSE, TOXIC, ZAP_CANNON, PSYCH_UP, HIDDEN_POWER, SUNNY_DAY, SNORE, HYPER_BEAM, PROTECT, RAIN_DANCE, ENDURE, FRUSTRATION, IRON_TAIL, RETURN, PSYCHIC_M, SHADOW_BALL, MUD_SLAP, DOUBLE_TEAM, SWAGGER, SLEEP_TALK, SWIFT, DREAM_EATER, DETECT, REST, ATTRACT, NIGHTMARE, CUT, FLASH
+	; end
