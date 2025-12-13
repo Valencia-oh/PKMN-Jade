def6296b4b2bba3572bc75b358186c38e892b87b
Valencia-oh <valencia_oh@protonmail.com>
2025-12-12 19:35:37 -0500

    Update menu_icon_pals.asm

--- data/pokemon/menu_icon_pals.asm
+++ data/pokemon/menu_icon_pals.asm
@@ -270,14 +270,18 @@ MonMenuIconPals:
 	icon_pals GREEN,  BLUE   ; ELECTRIKE
 	icon_pals BLUE,   GREEN  ; MANECTRIC
 	icon_pals BLUE,   PINK   ; HONCHKROW
-	icon_pals TEAL,  GREEN  ; BRONZOR
+	icon_pals TEAL,   GREEN  ; BRONZOR
 	icon_pals TEAL,   GREEN  ; BRONZONG
 	icon_pals RED,    BROWN  ; TOGEKISS
+	icon_pals GREEN,  GREEN  ; LEAFEON
+	icon_pals TEAL,   TEAL   ; GLACEON
 	icon_pals BROWN,  GREEN  ; MAMOSWINE
 	icon_pals GREEN,  BLUE   ; JOLTIK
 	icon_pals GREEN,  BLUE   ; GALVANTULA
 	icon_pals RED,    GREEN  ; LARVESTA
 	icon_pals RED,    GREEN  ; VOLCARONA
+	icon_pals BLUE,   BROWN  ; INKAY
+	icon_pals BLUE,   BROWN  ; MALAMAR
 	icon_pals RED,    GREEN  ; APPLIN
 	icon_pals RED,    GREEN  ; APPLETUN
 	icon_pals RED,    GREEN  ; FLAPPLE
