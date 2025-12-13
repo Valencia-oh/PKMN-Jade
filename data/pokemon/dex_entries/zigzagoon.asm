7b9e9147349574464a60286e7c5a97c3dafbbbc0
Valencia-oh <valencia_oh@protonmail.com>
2025-12-13 16:30:15 -0500

    Create zigzagoon.asm

---
+++ data/pokemon/dex_entries/zigzagoon.asm
@@ -0,0 +1,10 @@
+	db "TINY RACCOON@" ; species name
+	dw 104, 386 ; height, weight
+
+	db   "It walks in a"
+	next "zigzag fashion."
+	next "It is good at"
+
+	page "finding items in"
+	next "the grass and"
+	next "even in the ground.@"
