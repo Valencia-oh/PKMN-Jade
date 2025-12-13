9babb99ad79a86e42444abbc1d7e64cc134628aa
Valencia-oh <valencia_oh@protonmail.com>
2025-12-12 20:04:55 -0500

    Create leafeon.asm

---
+++ data/pokemon/dex_entries/leafeon.asm
@@ -0,0 +1,10 @@
+	db "VERDANT@" ; species name
+	dw 303, 562 ; height, weight
+
+	db   "Just like a plant"
+	next "it uses photosynthesis"
+	next "To get energy"
+
+	page "As a result"
+	next ", it is always"
+	next "covered in clean air.@"
