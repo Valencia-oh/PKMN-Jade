bb172878679cd77c8cf16acdf308456d8ce7e85c
Valencia-oh <valencia_oh@protonmail.com>
2025-12-12 19:59:28 -0500

    Create inkay.asm

---
+++ data/pokemon/dex_entries/inkay.asm
@@ -0,0 +1,10 @@
+	db "REVOLVING@" ; species name
+	dw 211, 430 ; height, weight
+
+	db   "It draws prey near"
+	next "with its blinking"
+	next "lights and then"
+
+	page "wraps them up in"
+	next "its long tentacles"
+	next "to hold in place.@"
