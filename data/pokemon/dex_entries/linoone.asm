c97355d74d60e6b1f90399b3e45316324721378c
Valencia-oh <valencia_oh@protonmail.com>
2025-12-13 16:28:22 -0500

    Create linoone.asm

---
+++ data/pokemon/dex_entries/linoone.asm
@@ -0,0 +1,10 @@
+	db "RUSHING@" ; species name
+	dw 108, 717 ; height, weight
+
+	db   "When running in"
+	next "a straight line,"
+	next "it can top 60"
+
+	page "miles per hour"
+	next "However, it has"
+	next "trouble with curves.@"
