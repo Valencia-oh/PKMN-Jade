7b0c87d9391046ce506d7f49a4f67149d4a0d7d6
Valencia-oh <valencia_oh@protonmail.com>
2025-12-13 16:00:35 -0500

    Update bitmasks.asm

--- gfx/pokemon/bitmasks.asm
+++ gfx/pokemon/bitmasks.asm
@@ -253,6 +253,8 @@ CelebiBitmasks:     INCLUDE "gfx/pokemon/celebi/bitmask.asm"
 
 PoochyenaBitmasks:  INCLUDE "gfx/pokemon/poochyena/bitmask.asm"
 MightyenaBitmasks:  INCLUDE "gfx/pokemon/mightyena/bitmask.asm"
+ZigzagoonBitmasks:  INCLUDE "gfx/pokemon/zizagoon/bitmask.asm"
+LinooneBitmasks:    INCLUDE "gfx/pokemon/linoone/bitmask.asm"
 WingullBitmasks:    INCLUDE "gfx/pokemon/wingull/bitmask.asm"
 PelipperBitmasks:   INCLUDE "gfx/pokemon/pelipper/bitmask.asm"
 ShroomishBitmasks:  INCLUDE "gfx/pokemon/shroomish/bitmask.asm"
