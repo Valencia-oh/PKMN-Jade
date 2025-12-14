79d787920ef49dc771d6dcccb4ad1f650cebb217
Valencia-oh <valencia_oh@protonmail.com>
2025-12-14 11:17:30 -0500

    Update map_constants.asm

--- constants/map_constants.asm
+++ constants/map_constants.asm
@@ -498,7 +498,7 @@ ENDM
 	map_const ROUTE_30_BERRY_HOUSE,                         4,  4 ;  9
 	map_const MR_POKEMONS_HOUSE,                            4,  4 ; 10
 	map_const ROUTE_31_VIOLET_GATE,                         5,  4 ; 11
-	map_const ROUTE_47,                                    31, 12 ; 12
+	map_const ROUTE_47,                                    21, 12 ; 12
 	endgroup
 
 DEF NUM_MAP_GROUPS EQU const_value ; 26
