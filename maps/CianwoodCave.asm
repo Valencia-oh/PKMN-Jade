898a985cb71408ff785eff5a3828a012a923b5de
Valencia-oh <valencia_oh@protonmail.com>
2025-12-13 22:01:41 -0500

    Update CianwoodCave.asm

--- maps/CianwoodCave.asm
+++ maps/CianwoodCave.asm
@@ -1,8 +1,12 @@
 	object_const_def
-	const CIANWOODCAVE_ROCK1
+
+CianwoodCave_MapScripts:
+	def_scene_scripts
+
+	def_callbacks
 
 
-CianwoodCity_MapEvents:
+CianwoodCave_MapEvents:
 	db 0, 0 ; filler
 
 	def_warp_events
@@ -10,5 +14,10 @@ CianwoodCity_MapEvents:
 
 	object_event  34, 14, SPRITE_ROCK, SPRITEMOVEDATA_SMASHABLE_ROCK, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, CianwoodCaveRock, -1
 
+	def_coord_events
+
+	def_bg_events
+
+	def_object_events
 
 	
