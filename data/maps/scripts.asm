699657dea80c7277ede2cd1168c9c8c13e6a9380
Valencia-oh <valencia_oh@protonmail.com>
2025-12-13 22:04:26 -0500

    Update scripts.asm

--- data/maps/scripts.asm
+++ data/maps/scripts.asm
@@ -67,6 +67,7 @@ INCLUDE "maps/PokecomCenterAdminOfficeMobile.asm"
 INCLUDE "maps/IlexForestAzaleaGate.asm"
 INCLUDE "maps/Route34IlexForestGate.asm"
 INCLUDE "maps/DayCare.asm"
+INCLUDE "maps/CianwoodCave.asm"
 
 
 SECTION "Map Scripts 5", ROMX
