743d12dd2ae6d752ba07cb8b484ed6626ab4c235
Valencia-oh <valencia_oh@protonmail.com>
2025-12-14 18:13:51 -0500

    Update johto_grass.asm

--- data/wild/johto_grass.asm
+++ data/wild/johto_grass.asm
@@ -1741,28 +1741,28 @@ def_grass_wildmons ROUTE_47
 	def_grass_wildmons CIANWOOD_CAVE
 	db 6 percent, 6 percent, 6 percent ; encounter rates: morn/day/nite
 	; morn
-	dbw 25, GEODUDE
-	dbw 26, SANDSHREW
-	dbw 25, ZUBAT
-	dbw 24, RATTATA
-	dbw 27, JOLTIK
-	dbw 26, ONIX
-	dbw 26, ONIX
+	dbw 25, GRAVELER
+	dbw 26, STARYU
+	dbw 25, KANGASKHAN
+	dbw 24, TANGELA
+	dbw 27, SHUCKLE
+	dbw 10, CHARMANDER
+	dbw 10, CYNDAQUIL
 	; day
-	dbw 26, GEODUDE
-	dbw 26, SANDSHREW
-	dbw 25, ZUBAT
-	dbw 24, RATTATA
-	dbw 27, JOLTIK
-	dbw 26, ONIX
-	dbw 26, ONIX
+	dbw 25, GRAVELER
+	dbw 26, STARYU
+	dbw 25, KANGASKHAN
+	dbw 24, TANGELA
+	dbw 27, SHUCKLE
+	dbw 10, CHARMANDER
+	dbw 10, CYNDAQUIL
 	; nite
-	dbw 26, GEODUDE
-	dbw 26, RATTATA
-	dbw 25, WOOPER
-	dbw 24, RATTATA
-	dbw 27, JOLTIK
-	dbw 26, ONIX
-	dbw 26, ONIX
+	dbw 25, GRAVELER
+	dbw 26, STARYU
+	dbw 25, KANGASKHAN
+	dbw 24, TANGELA
+	dbw 27, SHUCKLE
+	dbw 10, CHARMANDER
+	dbw 10, CYNDAQUIL
 	end_grass_wildmons
 	db -1 ; end
