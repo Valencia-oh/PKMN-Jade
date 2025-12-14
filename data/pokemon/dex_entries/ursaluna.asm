195dc46ae63f4f638b2923198a1c347ebb8e47f1
Valencia-oh <valencia_oh@protonmail.com>
2025-12-14 17:47:03 -0500

    Create ursaluna.asm

---
+++ data/pokemon/dex_entries/ursaluna.asm
@@ -0,0 +1,10 @@
+	db "PEAT@" ; species name
+	dw 710, 6393  ; height, weight
+
+	db   "Once common across"
+	next "Johto, Kanto and"
+	next "Sinnoh, it was"
+
+	page "slowly wiped out"
+	next "by the fear of "
+	next "humans.@"
