637e748a13bc616b5687379cf6d30e498bc62dd9
Valencia-oh <valencia_oh@protonmail.com>
2025-12-15 14:53:51 -0500

    Add files via upload

--- maps/CherryTowerF5.asm
+++ maps/CherryTowerF5.asm
@@ -3,7 +3,14 @@ object_const_def
 CherryTowerF5_MapScripts:
 	def_scene_scripts
 
+
+
 	def_callbacks
+Floor5Sign:
+	jumptext Floor5SignText
+
+Floor5SignText:
+	text "Teleport back down to Route 47"
 
 
 CherryTowerF5_MapEvents:
@@ -12,9 +19,12 @@ CherryTowerF5_MapEvents:
 	def_warp_events
 	warp_event 2, 2, CHERRY_TOWER_ROOF, 1
 	warp_event 2, 16, CHERRY_TOWER_F4, 2
+	warp_event 2, 0, Route 47, 1
+
 	def_coord_events
 
 	def_bg_events
+	bg_event  3, 0, BGEVENT_READ, Floor5Sign
 
 	def_object_events
 
