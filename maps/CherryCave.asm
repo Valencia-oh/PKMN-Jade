e7a996e08e1e198ce2dcb9bf9683a39e03989926
Valencia-oh <valencia_oh@protonmail.com>
2025-12-15 09:36:41 -0500

    Create CherryCave.asm

---
+++ maps/CherryCave.asm
@@ -0,0 +1,21 @@
+object_const_def
+
+CherryCave_MapScripts:
+	def_scene_scripts
+
+	def_callbacks
+
+
+CherryCave_MapEvents:
+	db 0, 0 ; filler
+
+	def_warp_events
+	warp_event 15,  10, ROUTE_47, 1
+	warp_event 6,  4, ROUTE_47, 2
+	def_coord_events
+
+	def_bg_events
+
+	def_object_events
+
+	
