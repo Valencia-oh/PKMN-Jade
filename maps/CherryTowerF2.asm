29fc264c69703c443920e946540777c012916b46
Valencia-oh <valencia_oh@protonmail.com>
2025-12-15 08:40:45 -0500

    Create CherryTowerF2.asm

---
+++ maps/CherryTowerF2.asm
@@ -0,0 +1,21 @@
+object_const_def
+
+CherryTowerF2_MapScripts:
+	def_scene_scripts
+
+	def_callbacks
+
+
+CherryTowerF2_MapEvents:
+	db 0, 0 ; filler
+
+	def_warp_events
+	warp_event 35, 17, CHERRY_TOWER_F3, 1
+	warp_event 35, 18, CHERRY_TOWER_F1, 1
+	def_coord_events
+
+	def_bg_events
+
+	def_object_events
+
+	
