a873a53da5757c18500bd40d2674224a27db11eb
Valencia-oh <valencia_oh@protonmail.com>
2025-12-15 09:19:40 -0500

    Update map_constants.asm

--- constants/map_constants.asm
+++ constants/map_constants.asm
@@ -155,13 +155,13 @@ ENDM
 	map_const SAFARI_ZONE_BETA,                            10, 18 ; 90
 	map_const VICTORY_ROAD,                                10, 36 ; 91
 	map_const CIANWOOD_CAVE,                               20, 10 ; 92
-	map_const CHERRY_TOWER_F1,                             10, 10 ; 42
-	map_const CHERRY_TOWER_F2,                             10, 10 ; 43
-	map_const CHERRY_TOWER_F3,                             10, 10 ; 44
-	map_const CHERRY_TOWER_F4,                             10, 10 ; 45
-	map_const CHERRY_TOWER_F5,                             10, 10 ; 46
-	map_const CHERRY_TOWER_ROOF,                           10, 10 ; 47
-	map_const CHERRY_CAVE,                                 20, 12 ; 48
+	map_const CHERRY_TOWER_F1,                             10, 10 ; 93
+	map_const CHERRY_TOWER_F2,                             10, 10 ; 94
+	map_const CHERRY_TOWER_F3,                             10, 10 ; 95
+	map_const CHERRY_TOWER_F4,                             10, 10 ; 96
+	map_const CHERRY_TOWER_F5,                             10, 10 ; 97
+	map_const CHERRY_TOWER_ROOF,                           10, 10 ; 98
+	map_const CHERRY_CAVE,                                 20, 12 ; 99
 	endgroup
 
 	newgroup ECRUTEAK                                             ;  4
