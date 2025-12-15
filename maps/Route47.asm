3ae603337e1bdf70ba0e32b34d14bc7421ef885a
Valencia-oh <valencia_oh@protonmail.com>
2025-12-15 09:50:10 -0500

    Update Route47.asm

--- maps/Route47.asm
+++ maps/Route47.asm
@@ -9,7 +9,7 @@ Route47_MapEvents:
 	db 0, 0 ; filler
 
 	def_warp_events
-	warp_event 11, 1, CHERRY_TOWER_F1, 1
+	warp_event 11, 1, CHERRY_TOWER_F1, 3
 	warp_event 11,  5, CHERRY_CAVE, 1
 	warp_event 11, 17, CHERRY_CAVE, 2
 	def_coord_events
