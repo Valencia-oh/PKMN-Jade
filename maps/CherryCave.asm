dcce2a5a9ce855a33e033f767802e2b65bfc9f7f
Valencia-oh <valencia_oh@protonmail.com>
2025-12-15 10:03:26 -0500

    Update CherryCave.asm

--- maps/CherryCave.asm
+++ maps/CherryCave.asm
@@ -10,8 +10,8 @@ CherryCave_MapEvents:
 	db 0, 0 ; filler
 
 	def_warp_events
-	warp_event 13,  9, ROUTE_47, 1
-	warp_event 31, 21, ROUTE_47, 2
+	warp_event 13,  9, ROUTE_47, 2
+	warp_event 31, 21, ROUTE_47, 1
 	def_coord_events
 
 	def_bg_events
