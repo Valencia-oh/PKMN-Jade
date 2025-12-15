a90ffead2000206413c1b6f4739ffd2ed9bff972
Valencia-oh <valencia_oh@protonmail.com>
2025-12-15 11:39:43 -0500

    Update item_constants.asm

--- constants/item_constants.asm
+++ constants/item_constants.asm
@@ -191,6 +191,7 @@ DEF FIRST_KEY_ITEM EQU const_value
 	const PASS         ; 0113
 	const SQUIRTBOTTLE ; 0114
 	const RAINBOW_WING ; 0115
+	const MEW_DNA ; 0116
 DEF NUM_KEY_ITEM_POCKET EQU const_value - FIRST_KEY_ITEM
 assert NUM_KEY_ITEM_POCKET <= $ff
 
