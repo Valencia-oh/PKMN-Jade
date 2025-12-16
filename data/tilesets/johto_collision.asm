028f4c91c030deca223fecc508048d1cf35e8b91
Valencia-oh <valencia_oh@protonmail.com>
2025-12-16 08:20:06 -0500

    Remove empty tile collision entries
    Removed empty tile collision entries for tiles 91 to 93.

--- data/tilesets/johto_collision.asm
+++ data/tilesets/johto_collision.asm
@@ -143,6 +143,3 @@
 	tilecoll FLOOR, FLOOR, FLOOR, FLOOR ; 8e
 	tilecoll FLOOR, FLOOR, FLOOR, FLOOR ; 8f
 	tilecoll WALL, WALL, WALL, WALL ; 90
-	tilecoll , , ,  ; 91
-	tilecoll , , ,  ; 92
-	tilecoll , , ,  ; 93
