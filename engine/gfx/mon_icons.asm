fc5a827fb81c1776bfe4c45015b920e935a165db
Valencia-oh <valencia_oh@protonmail.com>
2025-12-16 13:03:56 -0500

    Update mon_icons.asm

--- engine/gfx/mon_icons.asm
+++ engine/gfx/mon_icons.asm
@@ -499,11 +499,13 @@ GetIconBank:
 	lb bc, BANK("Mon Icons 1"), 8 ; Default Bank
 	ld a, h
 	cp HIGH(POOCHYENA) ; first species in "Mon Icons 3"
+	jr c, .check_2
 	ld a, l
 	cp LOW(POOCHYENA)
-	call GetPokemonIndexFromID
-	ld b, BANK("Mon Icons 3"),
+	jr c, .check_2
+	ld b, BANK("Mon Icons 3")
 	jr .return
+.check_2
 	ld a, h
 	cp HIGH(MAGIKARP) ; first species in "Mon Icons 2"
 	jr c, .return
