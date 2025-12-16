326b56533317cf3a5b528aaf619ad7a290825e62
Valencia-oh <valencia_oh@protonmail.com>
2025-12-16 16:21:20 -0500

    Update animations_gen2.asm

--- data/moves/animations_gen2.asm
+++ data/moves/animations_gen2.asm
@@ -85,6 +85,7 @@ BattleAnimationsGen2::
 	dw BattleAnim_RockSmash
 	dw BattleAnim_Whirlpool
 	dw BattleAnim_BeatUp
+	dw BattleAnim_Hail
 .IndirectEnd::
 
 BattleAnim_Sketch:
@@ -1768,4 +1769,19 @@ BattleAnimSub_Glimmer2:
 	anim_wait 5
 	anim_loop 2, .loop
 	anim_wait 16
+	anim_ret
+
+BattleAnim_Hail:
+	anim_1gfx BATTLE_ANIM_GFX_ICE
+	anim_bgeffect BATTLE_BG_EFFECT_WHITE_HUES, $0, $8, $0
+	anim_obj ANIM_OBJ_HAIL, 88, 0, $0
+	anim_wait 8
+	anim_obj ANIM_OBJ_HAIL, 72, 0, $1
+	anim_wait 8
+	anim_obj ANIM_OBJ_HAIL, 56, 0, $2
+.loop
+	anim_sound 0, 1, SFX_SHINE
+	anim_wait 8
+	anim_loop 8, .loop
+	anim_wait 8
 	anim_ret
