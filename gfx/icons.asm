a4950260c4bce3c2582bcc060703ebef7ab3dcc2
Valencia-oh <valencia_oh@protonmail.com>
2025-12-16 13:34:49 -0500

    Update icons.asm

--- gfx/icons.asm
+++ gfx/icons.asm
@@ -297,9 +297,6 @@ EspathraIcon::   INCBIN "gfx/icons/doduo.2bpp"
 PwooperIcon::    INCBIN "gfx/icons/pwooper.2bpp"
 ClodsireIcon::   INCBIN "gfx/icons/clodsire.2bpp"
 ChiyuIcon::      INCBIN "gfx/icons/chi_yu.2bpp"
-
-SECTION "Mon Icons 4", ROMX
-
 ErsrheadbbyIcon::INCBIN "gfx/icons/ersrheadbby.2bpp"
 EggIcon::        INCBIN "gfx/icons/egg.2bpp"
 
