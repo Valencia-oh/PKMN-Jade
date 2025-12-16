31755721a23af6774ed4b5ac8e09d289c98cf13d
Valencia-oh <valencia_oh@protonmail.com>
2025-12-15 22:23:54 -0500

    Create pwooper.asm

---
+++ data/pokemon/dex_entries/pwooper.asm
@@ -0,0 +1,10 @@
+	db "POISON FISH@" ; species name
+	dw 104, 187 ; height, weight
+
+	db   "Imported from"
+	next "Paldea for the"
+	next "decoration of the"
+
+	page "gardens of a king near"
+	next "Cianwood, they soon"
+	next "were invasive.@"
