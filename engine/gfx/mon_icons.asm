8349f98829718765e53b800a96688d3aca4819a1
Valencia-oh <valencia_oh@protonmail.com>
2025-12-16 13:28:37 -0500

    Update mon_icons.asm

--- engine/gfx/mon_icons.asm
+++ engine/gfx/mon_icons.asm
@@ -496,20 +496,32 @@ GetIconBank:
 	push hl
 	ld a, [wCurIcon]
 	call GetPokemonIndexFromID
+	lb bc, BANK("Mon Icons 1"), 8 ; Default Bank
 	ld a, h
-	cp HIGH(MAGIKARP) ; first species in "Mon Icons 2"
-	lb bc, BANK("Mon Icons 1"), 8
-	jr c, .return
+	cp HIGH(ERSRHEADBBY) ; first species in "Mon Icons 4"
+	jr c, .check_3
 	ld a, l
-	cp HIGH(POOCHYENA)
-	ld b, BANK("Mon Icons 2")
-	jr c, .return
-	ld a, l
-	cp LOW(POOCHYENA) ; first species in "Mon Icons 2"
-	lb bc, BANK("Mon Icons 3"), 8
-	jr c, .return
+	cp LOW(ERSRHEADBBY)
+	jr c, .check_3
+	ld b, BANK("Mon Icons 4")
+	jr .return
+.check_3
+	ld a, h
+	cp HIGH(POOCHYENA) ; first species in "Mon Icons 3"
+	jr c, .check_2
 	ld a, l
 	cp LOW(POOCHYENA)
+	jr c, .check_2
+	ld b, BANK("Mon Icons 3")
+	jr .return
+.check_2
+	ld a, h
+	cp HIGH(MAGIKARP) ; first species in "Mon Icons 2"
+	jr c, .return
+	ld a, l
+	cp LOW(MAGIKARP)
+	jr c, .return
+	ld b, BANK("Mon Icons 2")
 .return
 	pop hl
 	ret
