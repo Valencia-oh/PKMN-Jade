3fda951c2e13d2b4fb8ba8dfba11faca2b74c8e5
Valencia-oh <valencia_oh@protonmail.com>
2025-12-16 11:24:27 -0500

    Create trumbeak.asm

---
+++ data/pokemon/dex_entries/trumbeak.asm
@@ -0,0 +1,10 @@
+	db "BUGLE BEAK@" ; species name
+	dw 511, 1300 ; height, weight
+
+	db   "FILLER ENTRY"
+	next "FILLER ENTRY"
+	next "FILLER ENTRY"
+
+	page "FILLER ENTRY"
+	next "FILLER ENTRY"
+	next "FILLER ENTRY.@"
