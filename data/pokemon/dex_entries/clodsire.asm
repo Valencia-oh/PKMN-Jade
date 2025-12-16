65af49c98b4cf80e3658e1f0f6649afba758fea1
Valencia-oh <valencia_oh@protonmail.com>
2025-12-15 22:26:08 -0500

    Create clodsire.asm

---
+++ data/pokemon/dex_entries/clodsire.asm
@@ -0,0 +1,10 @@
+	db "SPINY FISH@" ; species name
+	dw 511, 4916 ; height, weight
+
+	db   "It lives at the "
+	next "bottom of ponds"
+	next " and swamps. It"
+
+	page "will carry Wooper"
+	next "on its back and "
+	next "Ferry Them.@"
