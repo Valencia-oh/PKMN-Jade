3376f46e066d6cfb14cbec69fb0350c5e86b2bde
Valencia-oh <valencia_oh@protonmail.com>
2025-12-16 15:28:02 -0500

    Update abilities.asm

--- data/abilities/abilities.asm
+++ data/abilities/abilities.asm
@@ -3,16 +3,12 @@
 AbilityDrizzleMons::
 	dw POLITOED
 	dw PELIPPER
-	dw LUVDISC
-	dw KYOGRE
 	dw -1
 
 AbilityDroughtMons::
 	dw NINETALES
 	dw MOLTRES
 	dw SUNFLORA
-	dw TORKOAL
-	dw GROUDON	
 	dw -1
 
 AbilitySandStreamMons::
@@ -21,15 +17,12 @@ AbilitySandStreamMons::
 	dw DUNSPARCE
 	dw TYRANITAR
 	dw TYRANITARX
-	dw REGIROCK
 	dw -1
 
 AbilitySnowWarningMons::
 	dw LAPRAS
 	dw ARTICUNO
 	dw DELIBIRD
-	dw REGICE
-	dw ANINETALES
 	dw -1
 
 AbilityIntimidateMons::
@@ -37,49 +30,28 @@ AbilityIntimidateMons::
 	dw ARCANINE
 	dw TAUROS
 	dw GYARADOS
-	dw GYARADOSX
 	dw GRANBULL
 	dw MIGHTYENA
 	dw MASQUERAIN
-	dw MAWILE
 	dw MANECTRIC
-	dw SALAMENCE
-	dw SALAMENCEX
-	dw TAUROS2
-	dw CYNDAQUIL2
-	dw QUILAVA2
-	dw TYPHLOSION2
 	dw -1
 
 AbilityMystifyMons::
 	dw CLEFABLE
 	dw WIGGLYTUFF
 	dw DRAGONITE
-	dw DRAGONITEX
-	dw DRAGONITEY
 	dw NOCTOWL
 	dw SLOWKING
 	dw MISDREAVUS
 	dw MISMAGIUS
 	dw UNOWN
-	dw SABLEYE
-	dw CHIMECHO
-	dw CHIMECHOX
-	dw MILOTIC
-	dw NOCTOWL2
-	dw NINETALES2
 	dw -1
 
 AbilitySnareMons::
 	dw VICTREEBEL
-	dw VICTREEBELX
 	dw TENTACRUEL
 	dw TANGELA
 	dw ARIADOS
-	dw CRADILY
-	dw TANGROWTH	
-	dw TENTACRUEL2
-	dw YANMA2
 	dw -1
 
 AbilityHasteMons::
@@ -87,12 +59,6 @@ AbilityHasteMons::
 	dw RAPIDASH
 	dw RAPIDASH2
 	dw DODRIO	
-	dw TORCHIC
-	dw COMBUSKEN
-	dw BLAZIKEN
-	dw NINJASK
-	dw SUNBOU
-	dw IKARI
 	dw -1
 
 AbilityAimMons::
@@ -100,12 +66,7 @@ AbilityAimMons::
 	dw PIDGEOTTO
 	dw PIDGEOT
 	dw ELECTABUZZ
-	dw ELECTIVIRE
 	dw MAGMAR
-	dw MAGMORTAR
-	dw AGEODUDE
-	dw AGRAVELER
-	dw AGOLEM
 	dw -1
 
 AbilityFocusMons::
@@ -121,9 +82,6 @@ AbilityVeiledMons::
 	dw TOGEPI
 	dw TOGETIC
 	dw TOGEKISS
-	dw GARDEVOIR
-	dw HANAMOLE
-	dw HANEEI
 	dw -1
 
 AbilityInvisibleWallMons::
@@ -141,7 +99,6 @@ AbilityDebrisMons::
 	dw FORRETRESS
 	dw QWILFISH
 	dw SKARMORY
-	dw SKARMORY2
 	dw -1
 
 ;Levitate/Waterproof mons ----------------------------------------------
@@ -155,17 +112,6 @@ AbilityLevitateMons::
 	dw GENGARX
 	dw KOFFING
 	dw WEEZING
-	dw VIBRAVA
-	dw FLYGON
-	dw LUNATONE
-	dw SOLROCK
-	dw BALTOY
-	dw CLAYDOL
-	dw DUSKULL
-	dw DUSCLOPS
-	dw MAGNEZONE
-	dw ARAICHU
-	dw FLYGONX
 	dw -1
 
 AbilityWaterproofMons::
@@ -173,11 +119,6 @@ AbilityWaterproofMons::
 	dw QUAGSIRE
 	dw SLUGMA
 	dw MAGCARGO
-	dw NUMEL
-	dw CAMERUPT
-	dw BARBOACH
-	dw WHISCASH
-	dw CAMERUPTX
 	dw -1
 
 ;Damage Boosting Ability Mons ----------------------------------------------
@@ -192,8 +133,6 @@ AbilityElementalFistMons::
 	dw LEDYBA
 	dw LEDIAN
 	dw BRELOOM
-	dw MEDICHAM
-	dw ANNIHILAPE
 	dw -1
 
 AbilityElementalFangMons::
@@ -204,16 +143,6 @@ AbilityElementalFangMons::
 	dw FERALIGATR
 	dw FURRET
 	dw LINOONE
-	dw CARVANHA
-	dw SHARPEDO
-	dw HUNTAIL
-	dw EXEGGUTOR2
-	dw FURRET2
-	dw TOTODILE2 
-	dw CROCONAW2 
-	dw FERALIGATR2
-	dw KOTORA
-	dw RAITORA
 	dw -1
 
 AbilityElementalBladeMons::	
@@ -223,17 +152,6 @@ AbilityElementalBladeMons::
 	dw SCIZOR
 	dw KABUTOPS
 	dw SNEASEL
-	dw TREECKO
-	dw GROVYLE
-	dw SCEPTILE
-	dw ABSOL
-	dw ABSOLX
-	dw GALLADE
-	dw WEAVILE
-	dw KLEAVOR
-	dw SCYTHER2
-	dw SCIZOR2
-	dw SCIZORX
 	dw -1
 
 AbilityStabilityMons::
@@ -249,7 +167,6 @@ AbilityStabilityMons::
 	dw PORYGON
 	dw ESPEON
 	dw UMBREON
-	dw SYLVEON
 	dw LEAFEON
 	dw GLACEON
 	dw CHIKORITA
@@ -260,11 +177,6 @@ AbilityStabilityMons::
 	dw TYPHLOSION
 	dw HITMONTOP
 	dw PORYGON2
-	dw PORYGONZ
-	dw KURUSU
-	dw AKUA
-	dw AKUERIA
-	dw ASHIBOMB
 	dw -1
 
 ;Healing Ability Mons ----------------------------------------------
@@ -272,42 +184,26 @@ AbilityStabilityMons::
 AbilityRegenerationMons:
 	dw GRIMER
 	dw MUK
-	dw AGRIMER
-	dw AMUK
 	dw STARYU
 	dw STARMIE
 	dw CHANSEY
 	dw BLISSEY
-	dw SWALOT
-	dw SWALOTX
 	dw -1
 
 AbilityRainDishMons::
 	dw SQUIRTLE
 	dw WARTORTLE
 	dw BLASTOISE
-	dw BLASTOISEX
 	dw SLOWBRO
-	dw SLOWBROX
-	dw LOTAD
-	dw LOMBRE
-	dw LUDICOLO
 	dw -1
 
 AbilitySunbaskMons::
 	dw BULBASAUR
 	dw IVYSAUR
 	dw VENUSAUR
-	dw VENUSAURX
 	dw CHARMANDER
 	dw CHARMELEON 
 	dw CHARIZARD
-	dw CHARIZARDX
-	dw TROPIUS
-	dw TROPIUSX
-	dw HONOBEA
-	dw BORUBEA
-	dw DYNABEA
 	dw -1
 
 AbilitySandBodyMons::
@@ -318,21 +214,10 @@ AbilitySandBodyMons::
 	dw SANDSLASH
 	dw MAROWAK
 	dw GLIGAR
-	dw GLISCOR
 	dw STEELIX
-	dw STEELIXX
 	dw -1
 
 AbilityIceBodyMons::
 	dw SEEL
 	dw DEWGONG
-	dw SPHEAL
-	dw SEALEO
-	dw WALREIN
-	dw SNORUNT
-	dw GLALIE
-	dw GLALIEX
-	dw FROSLASS
-	dw ASANDSHREW
-	dw ASANDSLASH
 	dw -1
