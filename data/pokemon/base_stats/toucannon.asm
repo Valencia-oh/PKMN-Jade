fe29c90bb63c038f62fd11890cb61ace6af89142
Valencia-oh <valencia_oh@protonmail.com>
2025-12-16 11:18:46 -0500

    Create toucannon.asm

---
+++ data/pokemon/base_stats/toucannon.asm
@@ -0,0 +1,20 @@
+	db 0 ; species ID placeholder
+
+	db  80,  120,  75,  60,  75,  75
+	evs  0,   3,   0,   0,   0,   0
+	;   hp  atk  def  spd  sat  sdf
+
+	db FIRE, FLYING ; type
+	db 45 ; catch rate
+	db 218 ; base exp
+	dw NO_ITEM, NO_ITEM ; items
+	db GENDER_F50 ; gender ratio
+	db 15 ; step cycles to hatch
+	INCBIN "gfx/pokemon/toucannon/front.dimensions"
+	dw NULL, NULL ; unused (beta front/back pics)
+	db GROWTH_MEDIUM_FAST ; growth rate
+	dn EGG_FLYING, EGG_FLYING ; egg groups
+
+	; tm/hm learnset
+	tmhm CURSE, TOXIC, HIDDEN_POWER, SUNNY_DAY, SNORE, HYPER_BEAM, PROTECT, ENDURE, FRUSTRATION, RETURN, MUD_SLAP, DOUBLE_TEAM, SWAGGER, SLEEP_TALK, SWIFT, DETECT, REST, ATTRACT, THIEF, STEEL_WING, FLAME_THROWER, FIRE_SPIN, FLY
+	; end
