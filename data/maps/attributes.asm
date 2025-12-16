39e99a31645fec0d4ecba5fca1810c7f93a210e2
Valencia-oh <valencia_oh@protonmail.com>
2025-12-16 08:54:30 -0500

    Update attributes.asm

--- data/maps/attributes.asm
+++ data/maps/attributes.asm
@@ -383,9 +383,13 @@ ENDM
 	connection west, Route32, ROUTE_32, -20
 	connection east, CherrygroveCity, CHERRYGROVE_CITY, 7
 
+	map_attributes CianwoodPalace, CIANWOOD_PALACE, $2c, SOUTH
+	connection south, Route48, ROUTE48, 0
+
+	map_attributes Route48, ROUTE_48, $2c, North
+	connection north, CianwoodPalace, CIANWOOD_PALACE, 0
 
 	map_attributes Route23, ROUTE_23, $0f, 0
-	map_attributes Route48, ROUTE_48, $0f, 0
 	map_attributes SproutTower1F, SPROUT_TOWER_1F, $00, 0
 	map_attributes SproutTower2F, SPROUT_TOWER_2F, $00, 0
 	map_attributes SproutTower3F, SPROUT_TOWER_3F, $00, 0
