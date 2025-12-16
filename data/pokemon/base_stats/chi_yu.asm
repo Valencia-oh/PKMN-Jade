8cb56fb547a3d16741e6f7a1d5ed98cd2c5970c5
Valencia-oh <valencia_oh@protonmail.com>
2025-12-16 11:13:07 -0500

    Create chi_yu.asm

---
+++ data/pokemon/base_stats/chi_yu.asm
@@ -0,0 +1,20 @@
+	db 0 ; species ID placeholder
+
+	db  55,  80,  80,  100, 135,  120
+	evs  0,   0,   0,   0,   2,   0
+	;   hp  atk  def  spd  sat  sdf
+
+	db DARK, FIRE ; type
+	db 6 ; catch rate
+	db 285 ; base exp
+	dw NO_ITEM, NO_ITEM ; items
+	db GENDER_F50 ; gender ratio
+	db 50 ; step cycles to hatch
+	INCBIN "gfx/pokemon/chi_yu/front.dimensions"
+	dw NULL, NULL ; unused (beta front/back pics)
+	db GROWTH_SLOW ; growth rate
+	dn EGG_NONE, EGG_NONE ; egg groups
+
+	; tm/hm learnset
+	tmhm HEADBUTT, CURSE, ROAR, TOXIC, ROCK_SMASH, HIDDEN_POWER, SUNNY_DAY, SNORE, HYPER_BEAM, PROTECT, ENDURE, FRUSTRATION, SOLARBEAM, IRON_TAIL, RETURN, SHADOW_BALL, MUD_SLAP, DOUBLE_TEAM, SWAGGER, SLEEP_TALK, SLUDGE_BOMB, FIRE_BLAST, SWIFT, DREAM_EATER, DETECT, REST, ATTRACT, THIEF, NIGHTMARE, STRENGTH, FLAMETHROWER
+	; end
