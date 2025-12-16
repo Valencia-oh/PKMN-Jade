832a1fc578fa9ab600458256ffbcec4870e14624
Valencia-oh <valencia_oh@protonmail.com>
2025-12-16 13:13:17 -0500

    Update mon_icons.asm

--- engine/gfx/mon_icons.asm
+++ engine/gfx/mon_icons.asm
@@ -503,7 +503,7 @@ GetIconBank:
 	ld a, l
 	cp LOW(POOCHYENA)
 	jr c, .check1
-	ld b, BANK("Mon Icons 3"), 8
+	ld b, BANK("Mon Icons 3"), 8 ; Default Bank
 	jr .return
 .check1
 	ld a, h
@@ -512,7 +512,7 @@ GetIconBank:
 	ld a, l
 	cp LOW(MAGIKARP)
 	jr c, .return
-	ld b, BANK("Mon Icons 2"), 8
+	ld b, BANK("Mon Icons 2"), 8 ; Default Bank
 .return
 	pop hl
 	ret
