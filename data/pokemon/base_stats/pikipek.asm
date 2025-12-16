243a4cc982e0217a9f0e75af5c840dfa7a9f9268
Valencia-oh <valencia_oh@protonmail.com>
2025-12-16 11:19:14 -0500

    Update pikipek.asm

--- data/pokemon/base_stats/pikipek.asm
+++ data/pokemon/base_stats/pikipek.asm
@@ -1,7 +1,7 @@
 	db 0 ; species ID placeholder
 
 	db  35,  75,  30,  56,  65,  30
-	evs  0,   0,   0,   1,   0,   0
+	evs  0,   1,   0,   0,   0,   0
 	;   hp  atk  def  spd  sat  sdf
 
 	db NORMAL, FLYING ; type
@@ -10,7 +10,7 @@
 	dw NO_ITEM, NO_ITEM ; items
 	db GENDER_F50 ; gender ratio
 	db 15 ; step cycles to hatch
-	INCBIN "gfx/pokemon/pidgey/front.dimensions"
+	INCBIN "gfx/pokemon/pikipek/front.dimensions"
 	dw NULL, NULL ; unused (beta front/back pics)
 	db GROWTH_MEDIUM_FAST ; growth rate
 	dn EGG_FLYING, EGG_FLYING ; egg groups
