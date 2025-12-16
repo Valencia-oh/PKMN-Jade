3dcb51676ac5a00fd5cdabeb6f5f367e77d9eb4e
Valencia-oh <valencia_oh@protonmail.com>
2025-12-16 12:39:53 -0500

    Add files via upload

--- maps/CianwoodPalace.asm
+++ maps/CianwoodPalace.asm
@@ -1,10 +1,46 @@
 	object_const_def
+const PALACE_CHI_YU
 
 CianwoodPalace_MapScripts:
 	def_scene_scripts
 
 	def_callbacks
+	callback MAPCALLBACK_OBJECTS, PalaceChiYuCallback
 
+PalaceChiYuCallback:
+	checkevent EVENT_SET_WHEN_FOUGHT_CHI_YU
+	iftrue .NoAppear
+	checkitem RUINED_BEADS
+	iftrue .Appear
+	sjump .NoAppear
+
+.Appear:
+	appear PALACE_CHI_YU
+	endcallback
+
+.NoAppear:
+	disappear PALACE_CHI_YU
+	endcallback
+
+PalaceChiYu:
+	faceplayer
+	opentext
+	writetext ChiyuText
+	cry MEW
+	pause 15
+	closetext
+	setevent EVENT_SET_WHEN_FOUGHT_CHI_YU
+	loadvar VAR_BATTLETYPE, BATTLETYPE_FORCEITEM
+	loadwildmon CHI_YU, 60
+	startbattle
+	disappear EVENT_PALACE_CHI_YU
+	reloadmapafterbattle
+	setevent EVENT_SET_WHEN_FOUGHT_CHI_YU
+	end
+
+ChiyuText:
+	text "Ch- Ch- CHI-YUUUU!"
+	done
 
 CianwoodPalace_MapEvents:
 	db 0, 0 ; filler
@@ -16,3 +52,5 @@ CianwoodPalace_MapEvents:
 	def_bg_events
 
 	def_object_events
+object_event  18,  17, SPRITE_CHI_YU, SPRITEMOVEDATA_POKEMON, 0, 0, -1, -1, PAL_NPC_RED, OBJECTTYPE_SCRIPT, 0, PalaceChiYU, EVENT_PALACE_CHI_YU
+
