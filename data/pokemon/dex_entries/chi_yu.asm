df72ed89d6863743bbde55d3c5b95fdbdb2592b1
Valencia-oh <valencia_oh@protonmail.com>
2025-12-16 11:29:09 -0500

    Create chi_yu.asm

---
+++ data/pokemon/dex_entries/chi_yu.asm
@@ -0,0 +1,10 @@
+	db "RUINOUS@" ; species name
+	dw 104, 108 ; height, weight
+
+	db   "It is said that"
+	next "the king of"
+	next "Cianwood once"
+
+	page "had this pokemon"
+	next "before it turned on him"
+	next "ruining the land.@"
