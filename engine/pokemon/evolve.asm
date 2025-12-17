76dc576079f3d1b4f8a419dcb920cd2fa8aeadc0
Valencia-oh <valencia_oh@protonmail.com>
2025-12-17 16:20:49 -0500

    Update evolve.asm

--- engine/pokemon/evolve.asm
+++ engine/pokemon/evolve.asm
@@ -196,7 +196,7 @@ EvolveAfterBattle_MasterLoop:
 	ld hl, EvolvingText
 	call PrintText
 
-	ld c, 25
+	ld c, 15
 	call DelayFrames
 
 	xor a
@@ -236,7 +236,7 @@ EvolveAfterBattle_MasterLoop:
 	call PlaySFX
 	call WaitSFX
 
-	ld c, 40
+	ld c, 15
 	call DelayFrames
 
 	call ClearTilemap
