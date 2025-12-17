2f2d52b77d2b9ff0d6d106efcc8a58102fe02946
Valencia-oh <valencia_oh@protonmail.com>
2025-12-17 18:50:32 -0500

    Update johto_collision.asm

--- data/tilesets/johto_collision.asm
+++ data/tilesets/johto_collision.asm
@@ -163,6 +163,3 @@
 	tilecoll FLOOR, TALL_GRASS, FLOOR, TALL_GRASS ; a2
 	tilecoll TALL_GRASS, TALL_GRASS, FLOOR, FLOOR ; a3
 	tilecoll WATER, FLOOR, WATER, FLOOR ; a4
-	tilecoll , , ,  ; a5
-	tilecoll , , ,  ; a6
-	tilecoll , , ,  ; a7
