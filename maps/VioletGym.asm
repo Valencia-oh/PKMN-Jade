53ccaaecd92ef67b73b31e0ce1cf9a1d61cfef2c
Valencia-oh <valencia_oh@protonmail.com>
2025-12-17 07:36:14 -0500

    Update VioletGym.asm

--- maps/VioletGym.asm
+++ maps/VioletGym.asm
@@ -31,7 +31,20 @@ VioletGymFalknerScript:
 	scall VioletGymActivateRockets
 .FightDone:
 	checkevent EVENT_GOT_TM31_MUD_SLAP
-	iftrue .SpeechAfterTM
+	readmem wBaseLevel
+	addval 4
+	writemem wBaseLevel
+
+	checkevent EVENT_LEVELCAPS_ENABLED
+	iffalse .SkipLevelCaps 
+	readmem wLevelCap
+	addval 4
+	writemem wLevelCap
+.SkipLevelCaps:
+
+	readmem wWildLevel
+	addval 4
+	writemem wWildLevel
 	setevent EVENT_BEAT_BIRD_KEEPER_ROD
 	setevent EVENT_BEAT_BIRD_KEEPER_ABE
 	setmapscene ELMS_LAB, SCENE_ELMSLAB_NOOP
