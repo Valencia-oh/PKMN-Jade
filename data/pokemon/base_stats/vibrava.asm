fe8681517864121996ae0592fbf1375a27c8527d
Valencia-oh <valencia_oh@protonmail.com>
2025-12-18 17:19:29 -0500

    Create vibrava.asm

---
+++ data/pokemon/base_stats/vibrava.asm
@@ -0,0 +1,20 @@
+	db 0 ; species ID placeholder
+
+	db  50,  70,  50,  70,  50,  50
+	evs  0,   1,   0,   1,   0,   0
+	;   hp  atk  def  spd  sat  sdf
+
+	db GROUND, DRAGON ; type
+	db 120 ; catch rate
+	db 126 ; base exp
+	dw NO_ITEM, NO_ITEM ; items
+	db GENDER_F50 ; gender ratio
+	db 20 ; step cycles to hatch
+	INCBIN "gfx/pokemon/trapinch/front.dimensions"
+	dw NULL, NULL ; unused (beta front/back pics)
+	db GROWTH_MEDIUM_SLOW ; growth rate
+	dn EGG_BUG, EGG_DRAGON ; egg groups
+
+	; tm/hm learnset
+	tmhm CURSE, TOXIC, ROCK_SMASH, HIDDEN_POWER, SUNNY_DAY, SNORE, PROTECT, ENDURE, FRUSTRATION, EARTHQUAKE, RETURN, DIG, MUD_SLAP, DOUBLE_TEAM, SWAGGER, SLEEP_TALK, SLUDGE_BOMB, REST, ATTRACT, THIEF, CUT, FLY, STRENGTH, 
+	; end
