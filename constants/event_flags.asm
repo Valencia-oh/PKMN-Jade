04168b46892df05c5c5192c10f8a5623dd6e246d
Valencia-oh <valencia_oh@protonmail.com>
2025-12-18 13:31:46 -0500

    Update event_flags.asm

--- constants/event_flags.asm
+++ constants/event_flags.asm
@@ -268,7 +268,8 @@
 	const EVENT_DANA_GAVE_THUNDERSTONE
 	const EVENT_TULLY_GAVE_WATER_STONE
 	const EVENT_TIFFANY_GAVE_PINK_BOW
-; Unused: next 339 events
+	const EVENT_LEVELCAPS_ENABLED
+; Unused: next 338 events
 
 	const_next 600
 ; Kurt Apricorn events
