798f94c1e6323ef7e425fc571586a22d21f51f9d
Valencia-oh <valencia_oh@protonmail.com>
2025-12-18 14:27:37 -0500

    Update MahoganyGym.asm

--- maps/MahoganyGym.asm
+++ maps/MahoganyGym.asm
@@ -62,11 +62,7 @@ MahoganyGymPryceScript:
 	readmem wBaseLevel
 	addval 5
 	writemem wBaseLevel
-	checkevent EVENT_LEVELCAPS_ENABLED
-	iffalse .SkipLevelCaps 
-	readmem wLevelCap
-	addval 5
-	writemem wLevelCap
+
 .SkipLevelCaps:
 	readmem wWildLevel
 	addval 5
