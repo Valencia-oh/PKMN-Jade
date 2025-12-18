c323011fc55f6af6bf9b96eb94ac5618001a66a2
Valencia-oh <valencia_oh@protonmail.com>
2025-12-18 17:23:06 -0500

    Create flygon.asm

---
+++ data/pokemon/dex_entries/flygon.asm
@@ -0,0 +1,10 @@
+	db "MYSTIC@" ; species name
+	dw 607, 1808 ; height, weight
+
+	db   "considered the"
+	next "most stunning"
+	next "dragon #MON by"
+
+	page "the cianwood king"
+	next "he had 17 of them"
+	next "imported to Johto.@"
