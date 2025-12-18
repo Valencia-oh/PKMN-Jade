ab498356774a969e436162c662b20991fd2d369d
Valencia-oh <valencia_oh@protonmail.com>
2025-12-18 14:26:46 -0500

    Update CianwoodGym.asm

--- maps/CianwoodGym.asm
+++ maps/CianwoodGym.asm
@@ -78,12 +78,7 @@ CianwoodGymChuckScript:
 	readmem wBaseLevel
 	addval 5
 	writemem wBaseLevel
-	checkevent EVENT_LEVELCAPS_ENABLED
-	iffalse .SkipLevelCaps 
-	readmem wLevelCap
-	addval 5
-	writemem wLevelCap
-.SkipLevelCaps:
+
 	readmem wWildLevel
 	addval 5
 	writemem wWildLevel
