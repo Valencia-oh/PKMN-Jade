989bb318103f0b4bb52e5effd76a3f684e14be6d
Valencia-oh <valencia_oh@protonmail.com>
2025-12-18 17:21:00 -0500

    Create flygon.asm

---
+++ data/pokemon/base_stats/flygon.asm
@@ -0,0 +1,20 @@
+	db 0 ; species ID placeholder
+
+	db  80,  100,  80,  100,  80,  80
+	evs  0,   1,   0,   2,   0,   0
+	;   hp  atk  def  spd  sat  sdf
+
+	db GROUND, DRAGON ; type
+	db 45 ; catch rate
+	db 234 ; base exp
+	dw NO_ITEM, NO_ITEM ; items
+	db GENDER_F50 ; gender ratio
+	db 20 ; step cycles to hatch
+	INCBIN "gfx/pokemon/flygon/front.dimensions"
+	dw NULL, NULL ; unused (beta front/back pics)
+	db GROWTH_MEDIUM_SLOW ; growth rate
+	dn EGG_BUG, EGG_DRAGON ; egg groups
+
+	; tm/hm learnset
+	tmhm CURSE, TOXIC, ROCK_SMASH, HIDDEN_POWER, SUNNY_DAY, SNORE, PROTECT, ENDURE, FRUSTRATION, EARTHQUAKE, RETURN, DIG, MUD_SLAP, DOUBLE_TEAM, SWAGGER, SLEEP_TALK, SLUDGE_BOMB, REST, ATTRACT, THIEF, CUT, FLY, STRENGTH
+	; end
