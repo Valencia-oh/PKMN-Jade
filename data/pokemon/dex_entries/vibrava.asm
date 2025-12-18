175942b7356f8a76020307180f39a7b62ab3af1b
Valencia-oh <valencia_oh@protonmail.com>
2025-12-18 17:24:29 -0500

    Create vibrava.asm

---
+++ data/pokemon/dex_entries/vibrava.asm
@@ -0,0 +1,10 @@
+	db "VIBRATION@" ; species name
+	dw 307, 337 ; height, weight
+
+	db   "While not native to"
+	next "Johto, they greatly"
+	next "enjoy the much more"
+
+	page "wet and lively"
+	next "climate compared"
+	next "to their deserts.@"
