3eae8e08c32b1590ba41c205fa28095c56fe52cf
Valencia-oh <valencia_oh@protonmail.com>
2025-12-18 17:26:04 -0500

    Create trapinch.asm

---
+++ data/pokemon/dex_entries/trapinch.asm
@@ -0,0 +1,10 @@
+	db "ANT PIT@" ; species name
+	dw 204, 331 ; height, weight
+
+	db   "It was once thought"
+	next "that if they got"
+	next "to the mainland"
+
+	page "their ravenous"
+	next "hunger would destroy"
+	next "the burned tower.@"
