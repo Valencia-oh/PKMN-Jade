e4938cba7d7c6fbb162a792053073611ae8123fc
Valencia-oh <valencia_oh@protonmail.com>
2025-12-18 12:24:28 -0500

    Update intro_menu.asm

--- engine/menus/intro_menu.asm
+++ engine/menus/intro_menu.asm
@@ -667,44 +667,6 @@ OakTextLevelCap:
 	text_far _OakLevelCapText
 	text_end
 
-SetLevelCap:
-	ld hl, OakTextLevelCap
-	call PrintText
-	ld hl, .LevelCapMenuHeader
-	call LoadMenuHeader
-	call VerticalMenu
-	call CloseWindow	
-	ld a, [wMenuCursorY]
-	cp $1
-	jr z, .LevelCapOff
-	cp $2
-	jr z, .LevelCapOn
-
-.LevelCapMenuHeader:
-	db MENU_BACKUP_TILES ; flags
-	menu_coords 0, 2, 15, TEXTBOX_Y - 1
-	dw .LevelCapMenuData
-	db 1 ; default option
-
-.LevelCapMenuData:
-	db STATICMENU_CURSOR ; flags
-	db 2 ; items
-	db "LevelCaps OFF@"
-	db "LevelCaps ON@"
-
-.LevelCapOn:
-    ld de, EVENT_LEVELCAPS_ENABLED
-    ld b, SET_FLAG
-	call EventFlagAction
-	ld a, 15
-	ld [wLevelCap], a
-	ret
-
-.LevelCapOff:
-	ld a, 100
-	ld [wLevelCap], a
-	ret
-
 NamePlayer:
 	farcall MovePlayerPicRight
 	farcall ShowPlayerNamingChoices
@@ -757,6 +719,45 @@ StorePlayerName:
 	ld hl, wPlayerName
 	ld de, wStringBuffer2
 	jmp CopyName2
+
+
+SetLevelCap:
+	ld hl, OakTextLevelCap
+	call PrintText
+	ld hl, .LevelCapMenuHeader
+	call LoadMenuHeader
+	call VerticalMenu
+	call CloseWindow	
+	ld a, [wMenuCursorY]
+	cp $1
+	jr z, .LevelCapOff
+	cp $2
+	jr z, .LevelCapOn
+
+.LevelCapMenuHeader:
+	db MENU_BACKUP_TILES ; flags
+	menu_coords 0, 2, 15, TEXTBOX_Y - 3
+	dw .LevelCapMenuData
+	db 1 ; default option
+
+.LevelCapMenuData:
+	db STATICMENU_CURSOR ; flags
+	db 2 ; items
+	db "LevelCaps OFF@"
+	db "LevelCaps ON@"
+
+.LevelCapOn:
+    ld de, EVENT_LEVELCAPS_ENABLED
+    ld b, SET_FLAG
+	Call EventFlagAction
+	ld a, 15
+	ld [wLevelCap], a
+	ret
+
+.LevelCapOff:
+	ld a, 100
+	ld [wLevelCap], a
+	ret
 
 ShrinkPlayer:
 	ldh a, [hROMBank]
