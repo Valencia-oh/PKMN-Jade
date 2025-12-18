e8d583145221afdfa749dd0c638ff6e829c33312
Valencia-oh <valencia_oh@protonmail.com>
2025-12-18 14:26:03 -0500

    Update EcruteakGym.asm

--- maps/EcruteakGym.asm
+++ maps/EcruteakGym.asm
@@ -73,12 +73,7 @@ EcruteakGymMortyScript:
 	readmem wBaseLevel
 	addval 5
 	writemem wBaseLevel
-	checkevent EVENT_LEVELCAPS_ENABLED
-	iffalse .SkipLevelCaps 
-	readmem wLevelCap
-	addval 5
-	writemem wLevelCap
-.SkipLevelCaps:
+
 	readmem wWildLevel
 	addval 5
 	writemem wWildLevel
