369452b8730f6807a513be0a20751e9ec9d38834
Valencia-oh <valencia_oh@protonmail.com>
2025-12-18 21:14:09 -0500

    Update mon_menu.asm

--- engine/pokemon/mon_menu.asm
+++ engine/pokemon/mon_menu.asm
@@ -1350,55 +1350,6 @@ String_MoveAcc:
 String_MoveEff:
 	db "EFF/@"
 
-; This converts values out of 256 into a value
-; out of 100. It achieves this by multiplying
-; the value by 100 and dividing it by 256.
-ConvertPercentages:
-
-	; Overwrite the "hl" register.
-	ld l, a
-	ld h, 0
-	push af
-
-	; Multiplies the value of the "hl" register by 3.
-	add hl, hl
-	add a, l
-	ld l, a
-	adc h
-	sub l
-	ld h, a
-
-	; Multiplies the value of the "hl" register
-	; by 8. The value of the "hl" register
-	; is now 24 times its original value.
-	add hl, hl
-	add hl, hl
-	add hl, hl
-
-	; Add the original value of the "hl" value to itself,
-	; making it 25 times its original value.
-	pop af
-	add a, l
-	ld l, a
-	adc h
-	sbc l
-	ld h, a
-
-	; Multiply the value of the "hl" register by
-	; 4, making it 100 times its original value.
-	add hl, hl
-	add hl, hl
-
-	; Set the "l" register to 0.5, otherwise the rounded
-	; value may be lower than expected. Round the
-	; high byte to nearest and drop the low byte.
-	ld l, 0.5
-	sla l
-	sbc a
-	and 1
-	add a, h
-	ret
-
 PlaceMoveScreenArrows:
 	call PlaceMoveScreenLeftArrow
 	jr PlaceMoveScreenRightArrow
