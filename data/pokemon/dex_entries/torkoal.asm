0fbe51bd5eb6e3b38dc09f0d5ce34311024bd06f
Valencia-oh <valencia_oh@protonmail.com>
2025-12-20 15:18:10 -0500

    Create torkoal.asm

---
+++ data/pokemon/dex_entries/torkoal.asm
@@ -0,0 +1,10 @@
+	db "COAL@" ; species name
+	dw 108, 1773 ; height, weight
+
+	db   "It burns coal "
+	next "inside its shell"
+	next "If it is attacked"
+
+	page "it belches thick"
+	next "black smoke "
+	next "and flees.@"
