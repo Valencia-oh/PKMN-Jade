1ae877bfcbab7eb728ae1bc9a4d2938344bd2236
Valencia-oh <valencia_oh@protonmail.com>
2025-12-20 00:11:51 -0500

    reimplement level caps to core

--- engine/battle/core.asm
+++ engine/battle/core.asm
@@ -7083,7 +7083,8 @@ GiveExperiencePoints:
 	ld [wCurSpecies], a
 	call GetBaseData
 	push bc
-	ld d, MAX_LEVEL
+	ld a, [wLevelCap]
+	ld d, a
 	farcall CalcExpAtLevel
 	pop bc
 	ld hl, MON_EXP + 2
@@ -7118,8 +7119,12 @@ GiveExperiencePoints:
 	pop bc
 	ld hl, MON_LEVEL
 	add hl, bc
+	ld a, [wLevelCap]
+	push bc
+	ld b, a
 	ld a, [hl]
-	cp MAX_LEVEL
+	cp b
+	pop bc
 	jmp nc, .next_mon
 	cp d
 	jmp z, .next_mon
@@ -7369,9 +7374,12 @@ AnimateExpBar:
 	ld a, [wCurBattleMon]
 	cp [hl]
 	jmp nz, .finish
-
+	ld a, [wLevelCap]
+	push bc
+	ld b, a
 	ld a, [wBattleMonLevel]
-	cp MAX_LEVEL
+	cp b
+	pop bc
 	jmp nc, .finish
 
 	ldh a, [hProduct + 3]
@@ -7408,7 +7416,8 @@ AnimateExpBar:
 	ld [hl], a
 
 .NoOverflow:
-	ld d, MAX_LEVEL
+	ld a, [wLevelCap]
+	ld d, a
 	farcall CalcExpAtLevel
 	ldh a, [hProduct + 1]
 	ld b, a
@@ -7443,8 +7452,12 @@ AnimateExpBar:
 	ld d, a
 
 .LoopLevels:
+	ld a, [wLevelCap]
+	push bc
+	ld b, a
 	ld a, e
-	cp MAX_LEVEL
+	cp b
+	pop bc
 	jr nc, .FinishExpBar
 	cp d
 	jr z, .FinishExpBar
