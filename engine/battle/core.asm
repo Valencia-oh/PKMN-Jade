f08c614e9ffaa74b5f7a29053e3c02df372b6378
Valencia-oh <valencia_oh@protonmail.com>
2025-12-20 01:16:41 -0500

    Update core.asm

--- engine/battle/core.asm
+++ engine/battle/core.asm
@@ -5508,14 +5508,18 @@ MoveInfoBox:
 	ld [wStringBuffer1], a
 	call .PrintPP
 
+	farcall UpdateMoveData
+	ld a, [wPlayerMoveStruct + MOVE_ANIM]
+	ld b, a
+	farcall GetMoveCategoryName
 	hlcoord 1, 9
-	ld de, .Type
+	ld de, wStringBuffer1
 	rst PlaceString
 
-	hlcoord 7, 11
+	ld h, b
+	ld l, c
 	ld [hl], '/'
 
-	farcall UpdateMoveData
 	ld a, [wPlayerMoveStruct + MOVE_ANIM]
 	ld b, a
 	hlcoord 2, 10
