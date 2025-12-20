34a0bfd3376eba44ea6496f288266ae50c76e5ef
Valencia-oh <valencia_oh@protonmail.com>
2025-12-19 19:43:02 -0500

    Update move_constants.asm

--- constants/move_constants.asm
+++ constants/move_constants.asm
@@ -257,9 +257,7 @@ DEF GEN1_MOVES EQU const_value - 1
 	const FUTURE_SIGHT ; f8
 	const ROCK_SMASH   ; f9
 	const WHIRLPOOL    ; fa
-	const BEAT_UP      ; fb
 ;ported over
-	const HAIL         ; fc
 	const POISON_TAIL  
 	const ASTONISH     
 	const THUNDER_FANG
