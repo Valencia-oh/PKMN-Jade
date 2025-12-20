08643ed9f7bc8544a46b1fcddf484434c40a8b57
Valencia-oh <valencia_oh@protonmail.com>
2025-12-20 00:06:44 -0500

    Update scripting.asm

--- engine/overworld/scripting.asm
+++ engine/overworld/scripting.asm
@@ -1089,9 +1089,9 @@ Script_loadwildmon:
 	call LoadScriptPokemonID
 	ld [wTempWildMonSpecies], a
 	rst GetScriptByte
-	;ld b, a
-	;ld a, [wWildLevel]
-	;add a, b
+	ld b, a
+	ld a, [wWildLevel]
+	add a, b
 	ld [wCurPartyLevel], a
 	ret
 
