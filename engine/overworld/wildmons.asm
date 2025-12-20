a021580062c827bdad4cc8ae41581e7b83feb02b
Valencia-oh <valencia_oh@protonmail.com>
2025-12-19 22:32:27 -0500

    Update wildmons.asm

--- engine/overworld/wildmons.asm
+++ engine/overworld/wildmons.asm
@@ -341,8 +341,9 @@ ChooseWildEncounter:
 	inc b
 ; Store the level
 .ok
-	ld a, [wWildLevel]
-	add a, b
+	;apply level scaling
+	;ld a, [wWildLevel]
+	;add a, b
 	ld [wCurPartyLevel], a
 
 	ld a, [hli]
