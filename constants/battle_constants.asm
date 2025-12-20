a0d90186ffd71f3f84f6ab605d67a9e267a46269
Valencia-oh <valencia_oh@protonmail.com>
2025-12-20 10:16:55 -0500

    Update battle_constants.asm

--- constants/battle_constants.asm
+++ constants/battle_constants.asm
@@ -208,7 +208,7 @@ DEF ALL_STATUS EQU (1 << PSN) | (1 << BRN) | (1 << FRZ) | (1 << PAR) | SLP_MASK
 ; wPlayerSubStatus5 or wEnemySubStatus5 bit flags
 	const_def
 	const SUBSTATUS_TOXIC
-	const_skip
+	const SUBSTATUS_STURDY
 	const_skip
 	const SUBSTATUS_TRANSFORMED
 	const SUBSTATUS_ENCORED
