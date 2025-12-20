590fb2514ded1474ddd6240c6cf573457f2b1da4
Valencia-oh <valencia_oh@protonmail.com>
2025-12-19 19:18:06 -0500

    Update core.asm

--- engine/battle/core.asm
+++ engine/battle/core.asm
@@ -7070,7 +7070,6 @@ GiveExperiencePoints:
 	ld [wApplyStatLevelMultipliersToEnemy], a
 	call ApplyStatLevelMultiplierOnAllStats
 	call ApplyStatusEffectOnPlayerStats
-	call BadgeStatBoosts
 	call UpdatePlayerHUD
 	call EmptyBattleTextbox
 	call LoadTilemapToTempTilemap
