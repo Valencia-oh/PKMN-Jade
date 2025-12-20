2d6488f80e91fb2c7d92dbde139fc13a90162740
Valencia-oh <valencia_oh@protonmail.com>
2025-12-19 19:43:20 -0500

    Update moves.asm

--- data/moves/moves.asm
+++ data/moves/moves.asm
@@ -279,7 +279,6 @@ MovesGen2:
 	move EFFECT_FUTURE_SIGHT,      200, PSYCHIC_TYPE,   SPECIAL,		100, 15,   0      ;FUTURE_SIGHT
 	move EFFECT_DEFENSE_DOWN_HIT,   55, FIGHTING,       PHYSICAL,		100, 15, 100      ;ROCK_SMASH
 	move EFFECT_TRAP_TARGET,        75, WATER,          SPECIAL,		100, 15,   0      ;WHIRLPOOL
-	move EFFECT_HAIL,		         0, ICE,            STATUS,			100, 10,   0      ;HAIL 
 	move EFFECT_FLINCH_HIT,         30, GHOST,          PHYSICAL,		100, 15,  30      ;ASTONISH
 	move EFFECT_POISON_HIT,         50, POISON,         PHYSICAL,		100, 25,  10      ;POISON_TAIL
 	move EFFECT_PARALYZE_HIT,       70, ELECTRIC,       PHYSICAL,		100, 20,  20      ;THUNDER_FANG
