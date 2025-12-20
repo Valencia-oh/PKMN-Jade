3d41c139f0e2b9474dfa4631f997272281b96b5e
Valencia-oh <valencia_oh@protonmail.com>
2025-12-19 19:56:47 -0500

    Update moves.asm

--- data/moves/moves.asm
+++ data/moves/moves.asm
@@ -224,7 +224,7 @@ MovesGen2:
 	move EFFECT_FORESIGHT,           0, NORMAL,         STATUS,			100, 40,   0      ;FORESIGHT
 	move EFFECT_DESTINY_BOND,        0, GHOST,          STATUS,			100,  5,   0      ;DESTINY_BOND
 	move EFFECT_PERISH_SONG,         0, NORMAL,         STATUS,			100,  5,   0      ;PERISH_SONG
-	move EFFECT_FREEZE,              0, ICE,            STATUS,			 90, 15, 100      ;ICY_WIND
+	move EFFECT_SPEED_DOWN_HIT,     55, ICE,            STATUS,			 95, 15, 100      ;ICY_WIND
 	move EFFECT_PROTECT,             0, FIGHTING,       STATUS,			100,  5,   0      ;DETECT
 	move EFFECT_MULTI_HIT,          25, GROUND,         PHYSICAL,		100, 10,   0      ;BONE_RUSH
 	move EFFECT_LOCK_ON,             0, NORMAL,         STATUS,			100,  5,   0      ;LOCK_ON
