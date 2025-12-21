55423c1e686daab2275e7ca48921abf25e33108d
Valencia-oh <valencia_oh@protonmail.com>
2025-12-21 14:32:28 -0500

    Update GoldenrodIsle.asm

--- maps/GoldenrodIsle.asm
+++ maps/GoldenrodIsle.asm
@@ -53,7 +53,7 @@ GoldenrodIsle_MapEvents:
 	def_bg_events
 
 	def_object_events
-object_event  5,  6, SPRITE_MOLTRES, SPRITEMOVEDATA_POKEMON, 0, 0, -1, -1, PAL_NPC_RED, OBJECTTYPE_SCRIPT, 0, CherryTowerMew, GOLDENROD_MOLTRES
+object_event  13,  11, SPRITE_MOLTRES, SPRITEMOVEDATA_POKEMON, 0, 0, -1, -1, PAL_NPC_RED, OBJECTTYPE_SCRIPT, 0, CherryTowerMew, GOLDENROD_MOLTRES
 
 
 
