71195ad35aced38a6564f2613f70d882c70a12f4
Valencia-oh <valencia_oh@protonmail.com>
2025-12-21 11:54:44 -0500

    Add files via upload

--- maps/Route47.asm
+++ maps/Route47.asm
@@ -9,9 +9,9 @@ Route47_MapEvents:
 	db 0, 0 ; filler
 
 	def_warp_events
-	warp_event 41, 13, CHERRY_TOWER_F1, 3
-	warp_event 48, 11, CHERRY_CAVE, 1
-	warp_event 52, 17, CHERRY_CAVE, 2
+	warp_event 19, 13, CHERRY_TOWER_F1, 3
+	warp_event 24, 11, CHERRY_CAVE, 1
+	warp_event 22, 17, CHERRY_CAVE, 2
 	def_coord_events
 
 	def_bg_events
