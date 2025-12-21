ffb74cbf27e98f4726d156c0db31279c91b04e61
Valencia-oh <valencia_oh@protonmail.com>
2025-12-21 14:15:30 -0500

    Update sprite_constants.asm

--- constants/sprite_constants.asm
+++ constants/sprite_constants.asm
@@ -148,6 +148,8 @@ DEF SPRITE_POKEMON EQU const_value
 	const SPRITE_CHI_YU; a3
 	const SPRITE_KAKUNA; a4
 	const SPRITE_METAPOD; a5
+	const SPRITE_SLOWKING; a6
+	const SPRITE_MOLTRES; a7
 DEF NUM_POKEMON_SPRITES EQU const_value - SPRITE_POKEMON
 
 ; special GetMonSprite values (see engine/overworld/overworld.asm)
