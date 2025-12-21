5054dc1221a3499a45ba3c9a589dd6eda583c77e
Valencia-oh <valencia_oh@protonmail.com>
2025-12-21 11:53:52 -0500

    Update map_constants.asm

--- constants/map_constants.asm
+++ constants/map_constants.asm
@@ -499,7 +499,7 @@ ENDM
 	newgroup CHERRYGROVE                                          ; 26
 	map_const ROUTE_30,                                    10, 27 ;  1
 	map_const ROUTE_31,                                    20,  9 ;  2
-	map_const CHERRYGROVE_CITY,                            20,  9 ;  3
+	map_const CHERRYGROVE_CITY,                            18,  9 ;  3
 	map_const CHERRYGROVE_MART,                             6,  4 ;  4
 	map_const CHERRYGROVE_POKECENTER_1F,                    5,  4 ;  5
 	map_const CHERRYGROVE_GYM_SPEECH_HOUSE,                 4,  4 ;  6
@@ -508,7 +508,7 @@ ENDM
 	map_const ROUTE_30_BERRY_HOUSE,                         4,  4 ;  9
 	map_const MR_POKEMONS_HOUSE,                            4,  4 ; 10
 	map_const ROUTE_31_VIOLET_GATE,                         5,  4 ; 11
-	map_const ROUTE_47,                                    21, 20 ; 12
+	map_const ROUTE_47,                                    14, 20 ; 12
 	endgroup
 
 DEF NUM_MAP_GROUPS EQU const_value ; 26
