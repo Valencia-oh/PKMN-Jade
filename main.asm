a3cef7ba09970629ff75e91643a18ab523e08fad
Valencia-oh <valencia_oh@protonmail.com>
2025-12-22 15:49:05 -0500

    Update main.asm

--- main.asm
+++ main.asm
@@ -171,8 +171,8 @@ INCLUDE "engine/pokemon/evolve.asm"
 SECTION "bank11", ROMX
 
 INCLUDE "engine/events/fruit_trees.asm"
-;INCLUDE "engine/battle/ai/move.asm"
-INCLUDE "engine/pokedex/pokedex_2.asm"
+INCLUDE "engine/battle/ai/move.asm"
+;INCLUDE "engine/pokedex/pokedex_2.asm"
 INCLUDE "engine/pokemon/mail.asm"
 
 
