c7497caed7675b2c7a05e439c20f1a82366d8e99
Valencia-oh <valencia_oh@protonmail.com>
2025-12-22 13:54:46 -0500

    Update mon_stats.asm

--- engine/pokemon/mon_stats.asm
+++ engine/pokemon/mon_stats.asm
@@ -463,3 +463,17 @@ ListMoves:
 	cp NUM_MOVES
 	jr nz, .nonmove_loop
 	ret
+
+GetMonTypeIndex:
+; type in c, because farcall clobbers a
+	ld a, c
+	; Skip Bird
+	cp BIRD
+	jr c, .done
+	cp UNUSED_TYPES
+	dec a
+	jr c, .done
+	sub UNUSED_TYPES
+.done
+	ld c, a
+	ret
