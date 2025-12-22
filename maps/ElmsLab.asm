181f89eeb0ce6e6eb226b0e372107a3712ac172e
Valencia-oh <valencia_oh@protonmail.com>
2025-12-22 14:55:49 -0500

    Update ElmsLab.asm

--- maps/ElmsLab.asm
+++ maps/ElmsLab.asm
@@ -1012,6 +1012,10 @@ if !DEF(_DEBUG)
 	line "true? Th-that's"
 	cont "incredible!"
 
+	para "The #DEX shows"
+	line "how #MON evolve"
+	cont "and learn moves."
+
 	para "He is superb at"
 	line "seeing the poten-"
 	cont "tial of people as"
