0ff0c8c036b00ad7ab86f561e82385c28fc5b0c5
Valencia-oh <valencia_oh@protonmail.com>
2025-12-21 23:02:29 -0500

    Update johto_collision.asm

--- data/johto_collision.asm
+++ data/johto_collision.asm
@@ -252,5 +252,5 @@
 	tilecoll WATER, BUOY, WHIRLPOOL, BUOY ; fb
 	tilecoll FLOOR, FLOOR, FLOOR, FLOOR ; fc
 	tilecoll WALL, FLOOR, FLOOR, FLOOR ; fd
-	tilecoll GRASS, FLOOR, FLOOR, FLOOR ; fe
-	tilecoll FLOOR, FLOOR, GRASS, GRASS ; ff
+	tilecoll TALL_GRASS, FLOOR, FLOOR, FLOOR ; fe
+	tilecoll FLOOR, FLOOR, TALL_GRASS, TALL_GRASS ; ff
