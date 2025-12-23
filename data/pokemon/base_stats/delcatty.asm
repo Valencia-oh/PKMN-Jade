01ecb62d4b44ee11723e77d2a6b3e0fa0c455021
Valencia-oh <valencia_oh@protonmail.com>
2025-12-23 12:03:26 -0500

    Create delcatty.asm

---
+++ data/pokemon/base_stats/delcatty.asm
@@ -0,0 +1,20 @@
+	db 0 ; species ID placeholder
+
+	db  70,  65,  65,  70,  55,  55
+	evs  1,   0,   0,   1,   0,   0
+	;   hp  atk  def  spd  sat  sdf
+
+	db NORMAL, DARK ; type
+	db 60 ; catch rate
+	db 140 ; base exp
+	dw MYSTERYBERRY, MOON_STONE ; items
+	db GENDER_F75 ; gender ratio
+	db 15 ; step cycles to hatch
+	INCBIN "gfx/pokemon/skitty/front.dimensions"
+	dw NULL, NULL ; unused (beta front/back pics)
+	db GROWTH_FAST ; growth rate
+	dn EGG_FAIRY, EGG_GROUND ; egg groups
+
+	; tm/hm learnset
+	tmhm DYNAMICPUNCH, HEADBUTT, CURSE, ROLLOUT, TOXIC, ZAP_CANNON, PSYCH_UP, HIDDEN_POWER, SUNNY_DAY, SNORE, BLIZZARD, PROTECT, RAIN_DANCE, ENDURE, FRUSTRATION, SOLARBEAM, IRON_TAIL, THUNDER, RETURN, PSYCHIC_M, SHADOW_BALL, MUD_SLAP, DOUBLE_TEAM, ICE_PUNCH, SWAGGER, SLEEP_TALK, FIRE_BLAST, DEFENSE_CURL, THUNDERPUNCH, DREAM_EATER, DETECT, REST, ATTRACT, FIRE_PUNCH, NIGHTMARE, STRENGTH, FLASH, FLAMETHROWER, THUNDERBOLT, ICE_BEAM
+	; end
