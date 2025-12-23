9140694c9a1a06226d3003e8e8658342dc3dd4a9
Valencia-oh <valencia_oh@protonmail.com>
2025-12-22 20:50:27 -0500

    Update mon_submenu.asm

--- engine/pokemon/mon_submenu.asm
+++ engine/pokemon/mon_submenu.asm
@@ -1,5 +1,131 @@
 INCLUDE "data/mon_menu.asm"
 
+
+	const_def
+	const OW_MOVE_FLASH ; 0
+	const OW_MOVE_SWEET_SCENT ; 1
+	const OW_MOVE_DIG ; 2
+	const OW_MOVE_FLY ; 3
+	const OW_MOVE_TELEPORT ; 4
+	const OW_MOVE_SOFTBOILED ; 5
+	const OW_MOVE_MILK_DRINK ; 6
+DEF NUM_MON_SUBMENU_MOVES EQU const_value ; 7 
+; DEF NO_PARAM_CHECK EQU -1
+DEF MON_SUBMENU_CONSUMES_PP EQU 0
+MACRO ow_submenu_move
+	db \1 ; actual move ID, cannot be -1/NO_PARAM_CHECK
+	db \2 ; MUST BE LEARNED and consumes PP, ie. for moves like Softboiled, NO_PARAM_CHECK otherwise (Softboiled, Milk Drink, *Heal Bell, *Recover, **Aromatherapy)
+	dw \3 ; engine_badge value; or -1 or NO_PARAM_CHECK for no badge check
+	db \4 ; item ID for TM/HM to check for presence in bag, -1 or NO_PARAM_CHECK for no TM/HM in bag check
+	assert \1 != NO_PARAM_CHECK, "A Move ID must be supplied"
+ENDM
+
+InteractableOverworldSubmenuMoves:
+	table_width 5, InteractableOverworldSubmenuMoves ; width is 4 because 1 byte for move ID, 2 for engine_badge state, 1 for item/TM/HM ID
+	ow_submenu_move FLASH, 			NO_PARAM_CHECK, ENGINE_ZEPHYRBADGE, HM_FLASH
+	ow_submenu_move SWEET_SCENT, 	NO_PARAM_CHECK, NO_PARAM_CHECK, 	TM_SWEET_SCENT
+	; ow_submenu_move DIG, 			NO_PARAM_CHECK, NO_PARAM_CHECK, 	TM_DIG
+	; ow_submenu_move FLY, 			NO_PARAM_CHECK, ENGINE_STORMBADGE, 	HM_FLY
+	ow_submenu_move DIG, 			NO_PARAM_CHECK, NO_PARAM_CHECK, 	NO_PARAM_CHECK
+	ow_submenu_move FLY, 			NO_PARAM_CHECK, NO_PARAM_CHECK, 	NO_PARAM_CHECK	
+	ow_submenu_move TELEPORT, 		NO_PARAM_CHECK, NO_PARAM_CHECK, 	NO_PARAM_CHECK
+	ow_submenu_move SOFTBOILED, 	MON_SUBMENU_CONSUMES_PP, 	NO_PARAM_CHECK, 	NO_PARAM_CHECK
+	ow_submenu_move MILK_DRINK, 	MON_SUBMENU_CONSUMES_PP, 	NO_PARAM_CHECK, 	NO_PARAM_CHECK
+	assert_table_length NUM_MON_SUBMENU_MOVES
+
+TryOW_MonMenu:
+	; given index in 'a'
+	ld c, 5 ; index * 5 since each entry is 4 bytes
+	call SimpleMultiply ; Return a * c.
+	ld hl, InteractableOverworldSubmenuMoves
+	ld d, 0
+	ld e, a
+	add hl, de ; hl is now pointing to ow_move \1, the move ID
+	ld a, [hli]
+	ld [wPutativeTMHMMove], a ; move ID will be safe here and used later	
+
+	ld a, [hli] ; consume_PP check
+	ld [wTempByteValue], a
+
+	ld e, [hl] ; lower half of ow_move \2, engine_badge
+	inc hl
+	ld d, [hl] ; upper half of ow_move \2, engine_badge
+	inc hl     ; hl now pointing to ow_move \3, the TM/HM item if any
+	ld a, NO_PARAM_CHECK ; -1
+	cp e
+	jr nz, .badge_test
+	cp d
+	jr z, .badge_passed	; no badge req
+.badge_test
+	push hl
+	; call CheckEngineFlag
+	; Check engine flag de
+	ld b, CHECK_FLAG
+	farcall EngineFlagAction
+	ld a, c
+	and a
+	; jr nz, .isset
+	jr z, .badge_fail
+	pop hl
+.badge_passed
+	ld a, [hl] ; TM/HM associated
+	cp NO_PARAM_CHECK ; -1 ; to see if we should skip this check
+	jr z, .check_knownmoves
+	ld [wCurItem], a
+	ld hl, wNumItems
+	call CheckItem
+	jr z, .fail
+
+.check_knownmoves	
+	ld a, MON_MOVES
+	call GetPartyParamLocation
+	ld a, [wPutativeTMHMMove]
+	ld b, a
+	ld c, NUM_MOVES
+.checkknownloop
+	ld a, [hli]
+	cp b
+	jr z, .sucess
+	dec c
+	jr nz, .checkknownloop
+
+; check if we're a PP consuming move, if so, quit because we dont currently have the move learned
+; wTempByteValue/wNamedObjectIndex is going to be overwritten so we have to do this now
+	ld a, [wTempByteValue]
+	cp MON_SUBMENU_CONSUMES_PP
+	jr z, .fail
+
+.check_learnset
+	; level up, doesnt matter what current lvl you are
+	predef CanLearnViaLvlUp
+	ld a, [wNamedObjectIndex]
+	ld b, a
+	ld a, [wPutativeTMHMMove]
+  	cp b
+	jr z, .sucess
+	; egg moves
+	predef CanLearnViaEggMove
+	ld a, [wNamedObjectIndex]
+	ld b, a
+	ld a, [wPutativeTMHMMove]
+  	cp b
+	jr z, .sucess
+	; TM/HM/Move Tutor
+	predef CanLearnTMHMMove ; returns result in c, 0 if cannot learn
+	ld a, c
+	and a
+	jr z, .fail
+
+.sucess
+	xor a
+	ret
+.fail
+	ld a, -1
+	ret
+.badge_fail
+	pop hl
+	jr .fail
+
 MonSubmenu:
 	xor a
 	ldh [hBGMapMode], a
@@ -115,14 +241,7 @@ GetMonSubmenuItems:
 	ld a, [wLinkMode]
 	and a
 	jr nz, .skip_moves
-
-	call CanUseFlash
-	call CanUseFly
-	call CanUseDig
-	call Can_Use_Sweet_Scent
-	call CanUseTeleport
-	call CanUseSoftboiled
-	call CanUseMilkdrink
+	call Iterate_OW_Submenu_Moves
 
 .skip_moves
 	ld a, MONMENUITEM_STATS
