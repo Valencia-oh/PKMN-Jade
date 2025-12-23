b35bfb0ab4873da32405d44694f01afe1a1a66b0
Valencia-oh <valencia_oh@protonmail.com>
2025-12-23 16:44:44 -0500

    Create feebas.asm

---
+++ data/pokemon/dex_entries/feebas.asm
@@ -0,0 +1,10 @@
+	db "FISH@" ; species name
+	dw 200, 163 ; height, weight
+
+	db   "Long thought to be"
+	next "an evolutionary"
+	next "dead end, professors"
+
+	page "often fear that it"
+	next "is on the verge of"
+	next "extinction in Johto.@"
