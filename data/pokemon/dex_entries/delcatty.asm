e9446fc925eee777130feb7122ec57771c98c1b2
Valencia-oh <valencia_oh@protonmail.com>
2025-12-23 11:59:26 -0500

    Create delcatty.asm

---
+++ data/pokemon/dex_entries/delcatty.asm
@@ -0,0 +1,10 @@
+	db "PRIM@" ; species name
+	dw 307, 719 ; height, weight
+
+	db   "It has long been"
+	next "a fascination of the"
+	next "wealthy of Johto and"
+
+	page "Kanto, so much in"
+	next "fact that 87 were"
+	next "imported 50 years ago.@"
