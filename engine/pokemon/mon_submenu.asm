64ca10fbb2f6cdedda1346c146a167a30c73805e
Valencia-oh <valencia_oh@protonmail.com>
2025-12-22 21:25:22 -0500

    Update mon_submenu.asm

--- engine/pokemon/mon_submenu.asm
+++ engine/pokemon/mon_submenu.asm
@@ -1,242 +1,5 @@
 INCLUDE "data/mon_menu.asm"
 
-
-	const_def
-	const OW_MOVE_FLASH ; 0
-	const OW_MOVE_SWEET_SCENT ; 1
-	const OW_MOVE_DIG ; 2
-	const OW_MOVE_FLY ; 3
-	const OW_MOVE_TELEPORT ; 4
-	const OW_MOVE_SOFTBOILED ; 5
-	const OW_MOVE_MILK_DRINK ; 6
-DEF NUM_MON_SUBMENU_MOVES EQU const_value ; 7 
-; DEF NO_PARAM_CHECK EQU -1
-DEF MON_SUBMENU_CONSUMES_PP EQU 0
-MACRO ow_submenu_move
-	db \1 ; actual move ID, cannot be -1/NO_PARAM_CHECK
-	db \2 ; MUST BE LEARNED and consumes PP, ie. for moves like Softboiled, NO_PARAM_CHECK otherwise (Softboiled, Milk Drink, *Heal Bell, *Recover, **Aromatherapy)
-	dw \3 ; engine_badge value; or -1 or NO_PARAM_CHECK for no badge check
-	db \4 ; item ID for TM/HM to check for presence in bag, -1 or NO_PARAM_CHECK for no TM/HM in bag check
-	assert \1 != NO_PARAM_CHECK, "A Move ID must be supplied"
-ENDM
-
-InteractableOverworldSubmenuMoves:
-	table_width 5, InteractableOverworldSubmenuMoves ; width is 4 because 1 byte for move ID, 2 for engine_badge state, 1 for item/TM/HM ID
-	ow_submenu_move FLASH, 			NO_PARAM_CHECK, ENGINE_ZEPHYRBADGE, HM_FLASH
-	ow_submenu_move SWEET_SCENT, 	NO_PARAM_CHECK, NO_PARAM_CHECK, 	TM_SWEET_SCENT
-	; ow_submenu_move DIG, 			NO_PARAM_CHECK, NO_PARAM_CHECK, 	TM_DIG
-	; ow_submenu_move FLY, 			NO_PARAM_CHECK, ENGINE_STORMBADGE, 	HM_FLY
-	ow_submenu_move DIG, 			NO_PARAM_CHECK, NO_PARAM_CHECK, 	NO_PARAM_CHECK
-	ow_submenu_move FLY, 			NO_PARAM_CHECK, NO_PARAM_CHECK, 	NO_PARAM_CHECK	
-	ow_submenu_move TELEPORT, 		NO_PARAM_CHECK, NO_PARAM_CHECK, 	NO_PARAM_CHECK
-	ow_submenu_move SOFTBOILED, 	MON_SUBMENU_CONSUMES_PP, 	NO_PARAM_CHECK, 	NO_PARAM_CHECK
-	ow_submenu_move MILK_DRINK, 	MON_SUBMENU_CONSUMES_PP, 	NO_PARAM_CHECK, 	NO_PARAM_CHECK
-	assert_table_length NUM_MON_SUBMENU_MOVES
-
-TryOW_MonMenu:
-	; given index in 'a'
-	ld c, 5 ; index * 5 since each entry is 4 bytes
-	call SimpleMultiply ; Return a * c.
-	ld hl, InteractableOverworldSubmenuMoves
-	ld d, 0
-	ld e, a
-	add hl, de ; hl is now pointing to ow_move \1, the move ID
-	ld a, [hli]
-	ld [wPutativeTMHMMove], a ; move ID will be safe here and used later	
-
-	ld a, [hli] ; consume_PP check
-	ld [wTempByteValue], a
-
-	ld e, [hl] ; lower half of ow_move \2, engine_badge
-	inc hl
-	ld d, [hl] ; upper half of ow_move \2, engine_badge
-	inc hl     ; hl now pointing to ow_move \3, the TM/HM item if any
-	ld a, NO_PARAM_CHECK ; -1
-	cp e
-	jr nz, .badge_test
-	cp d
-	jr z, .badge_passed	; no badge req
-.badge_test
-	push hl
-	; call CheckEngineFlag
-	; Check engine flag de
-	ld b, CHECK_FLAG
-	farcall EngineFlagAction
-	ld a, c
-	and a
-	; jr nz, .isset
-	jr z, .badge_fail
-	pop hl
-.badge_passed
-	ld a, [hl] ; TM/HM associated
-	cp NO_PARAM_CHECK ; -1 ; to see if we should skip this check
-	jr z, .check_knownmoves
-	ld [wCurItem], a
-	ld hl, wNumItems
-	call CheckItem
-	jr z, .fail
-
-.check_knownmoves	
-	ld a, MON_MOVES
-	call GetPartyParamLocation
-	ld a, [wPutativeTMHMMove]
-	ld b, a
-	ld c, NUM_MOVES
-.checkknownloop
-	ld a, [hli]
-	cp b
-	jr z, .sucess
-	dec c
-	jr nz, .checkknownloop
-
-; check if we're a PP consuming move, if so, quit because we dont currently have the move learned
-; wTempByteValue/wNamedObjectIndex is going to be overwritten so we have to do this now
-	ld a, [wTempByteValue]
-	cp MON_SUBMENU_CONSUMES_PP
-	jr z, .fail
-
-.check_learnset
-	; level up, doesnt matter what current lvl you are
-	predef CanLearnViaLvlUp
-	ld a, [wNamedObjectIndex]
-	ld b, a
-	ld a, [wPutativeTMHMMove]
-  	cp b
-	jr z, .sucess
-	; TM/HM/Move Tutor
-	predef CanLearnTMHMMove ; returns result in c, 0 if cannot learn
-	ld a, c
-	and a
-	jr z, .fail
-
-.sucess
-	xor a
-	ret
-.fail
-	ld a, -1
-	ret
-.badge_fail
-	pop hl
-	jr .fail
-
-Iterate_OW_Submenu_Moves:
-	; this reflects the order that the moves will appear stacked on each other if multiple are added
-	call CanUseFlash
-	call CanUseFly
-	call CanUseDig
-	call Can_Use_Sweet_Scent
-	call CanUseTeleport
-	call CanUseSoftboiled
-	call CanUseMilkdrink
-	ret
-
-CanUseFlash:
-; Location Check
-	farcall SpecialAerodactylChamber
-	jr c, .valid_location ; can use flash
-	ld a, [wTimeOfDayPalset]
-	cp DARKNESS_PALSET
-	ret nz ; .fail ; not a darkcave
-
-.valid_location
-	ld a, OW_MOVE_FLASH
-	call TryOW_MonMenu
-	and a
-	ret nz ; if not zero, do not add
-; found
-	ld a, MONMENUITEM_FLASH
-	call AddMonMenuItem
-	ret
-
-Can_Use_Sweet_Scent:
-	farcall CanEncounterWildMon
-	ret nc
-	farcall GetMapEncounterRate
-	ld a, b
-	and a
-	ret z
-
-	ld a, OW_MOVE_SWEET_SCENT
-	call TryOW_MonMenu
-	and a
-	ret nz ; if not zero, do not add
-
-	ld a, MONMENUITEM_SWEETSCENT
-	call AddMonMenuItem
-	ret
-
-CanUseDig:
-	call GetMapEnvironment
-	cp CAVE
-	jr z, .valid_location
-	cp DUNGEON
-	ret nz ; fail, not inside cave or dungeon
-
-.valid_location
-	ld a, OW_MOVE_DIG
-	call TryOW_MonMenu
-	and a
-	ret nz ; if not zero, do not add
-; found
-	ld a, MONMENUITEM_DIG
-	call AddMonMenuItem
-	ret
-
-CanUseFly:
-	call GetMapEnvironment
-	call CheckOutdoorMap
-	ret nz ; not outdoors, cant fly
-	
-	ld a, OW_MOVE_FLY
-	call TryOW_MonMenu
-	and a
-	ret nz ; if not zero, do not add
-; found
-	ld a, MONMENUITEM_FLY
-	call AddMonMenuItem
-	ret
-
-CanUseTeleport:
-	call GetMapEnvironment
-	call CheckOutdoorMap
-	jr z, .valid_location
-	
-	call GetMapEnvironment
-	cp CAVE
-	jr z, .valid_location
-	cp DUNGEON
-	ret nz ; last valid location
-
-.valid_location
-	ld a, OW_MOVE_TELEPORT
-	call TryOW_MonMenu
-	and a
-	ret nz ; if not zero, do not add
-
-	ld a, MONMENUITEM_TELEPORT
-	call AddMonMenuItem	
-	ret
-
-CanUseSoftboiled:
-	ld a, OW_MOVE_SOFTBOILED
-	call TryOW_MonMenu
-	and a
-	ret nz ; if not zero, do not add
-	ld a, MONMENUITEM_SOFTBOILED
-	call AddMonMenuItem
-	ret
-
-CanUseMilkdrink:
-	ld a, OW_MOVE_MILK_DRINK
-	call TryOW_MonMenu
-	and a
-	ret nz ; if not zero, do not add
-
-	ld a, MONMENUITEM_MILKDRINK
-	call AddMonMenuItem
-	ret
-
-
 MonSubmenu:
 	xor a
 	ldh [hBGMapMode], a
@@ -352,7 +115,14 @@ GetMonSubmenuItems:
 	ld a, [wLinkMode]
 	and a
 	jr nz, .skip_moves
-	call Iterate_OW_Submenu_Moves
+
+	call CanUseFlash
+	call CanUseFly
+	call CanUseDig
+	call Can_Use_Sweet_Scent
+	call CanUseTeleport
+	call CanUseSoftboiled
+	call CanUseMilkdrink
 
 .skip_moves
 	ld a, MONMENUITEM_STATS
@@ -493,15 +263,282 @@ BattleMonMenu:
 	db "SWITCH@"
 	db "STATS@"
 	db "CANCEL@"
+
 CheckMonCanLearn_TM_HM:
 ; Check if wCurPartySpecies can learn move in 'a'
 	ld [wPutativeTMHMMove], a
 	ld a, [wCurPartySpecies]
-farcall CanLearnTMHMMove
+	farcall CanLearnTMHMMove
 .check
 	ld a, c
 	and a
 	ret z
 ; yes
 	scf
+	ret
+
+CheckMonKnowsMove:
+	ld b, a
+	ld a, MON_MOVES
+	call GetPartyParamLocation
+	ld d, h
+	ld e, l
+	ld c, NUM_MOVES
+.loop
+	ld a, [de]
+	and a
+	jr z, .next
+	cp b
+	jr z, .found ; knows move
+.next
+	inc de
+	dec c
+	jr nz, .loop
+	ld a, -1
+	scf ; mon doesnt know move
+	ret
+.found
+	xor a
+	ret z
+
+CheckLevelUpMoves:
+	ld d, a
+	ld a, [wTempSpecies]
+	call GetPokemonIndexFromID
+	ld b, h
+	ld c, l
+	ld hl, EvosAttacksPointers
+	ld a, BANK(EvosAttacksPointers)
+	call LoadDoubleIndirectPointer
+	ld [wStatsScreenFlags], a ; bank
+	call FarSkipEvolutions
+.learnset_loop
+	call GetFarByte
+  	and a
+	jr z, .notfound
+	inc hl
+	call GetFarWord
+	call GetMoveIDFromIndex
+	cp d
+	jr z, .found
+	inc hl
+	inc hl
+	jr .learnset_loop
+
+.found
+	xor a
+	ret ; move is in lvl up learnset
+.notfound
+	scf ; move isnt in lvl up learnset
+	ret
+
+CanUseFlash:
+; Step 1: Badge Check
+	ld de, ENGINE_ZEPHYRBADGE
+	ld b, CHECK_FLAG
+	farcall EngineFlagAction
+	ld a, c
+	and a
+	ret z ; .fail, dont have needed badge
+
+; Step 2: Location Check
+	farcall SpecialAerodactylChamber
+	jr c, .valid_location ; can use flash
+	ld a, [wTimeOfDayPalset]
+	cp DARKNESS_PALSET
+	ret nz ; .fail ; not a darkcave
+
+.valid_location
+; Step 3: Check if Mon knows Move
+	ld a, FLASH
+	call CheckMonKnowsMove
+	and a
+	jr z, .yes
+
+; Step 4: Check for TM/HM in bag
+	ld a, HM_FLASH
+	ld [wCurItem], a
+	ld hl, wNumItems
+	call CheckItem
+	ret nc ; hm isnt in bag
+
+; Step 5: Check if Mon can learn move from TM/HM/Move Tutor
+	ld a, FLASH
+	call CheckMonCanLearn_TM_HM
+	jr c, .yes
+
+; Step 6: Check if Mon can learn move from LVL-UP
+	ld a, FLASH
+	call CheckLvlUpMoves
+	ret c ; fail
+
+.yes
+	ld a, MONMENUITEM_FLASH
+	call AddMonMenuItem
+	ret
+
+CanUseFly:
+; Step 1: Badge Check
+	ld de, ENGINE_STORMBADGE
+	ld b, CHECK_FLAG
+	farcall EngineFlagAction
+	ld a, c
+	and a
+	ret z ; .fail, dont have needed badge
+
+; Step 2: Location Check
+	call GetMapEnvironment
+	call CheckOutdoorMap
+	ret nz ; not outdoors, cant fly
+
+; Step 3: Check if Mon knows Move
+	ld hl, FLY
+	call GetMoveIDFromIndex
+	call CheckMonKnowsMove
+	and a
+	jr z, .yes
+
+; Step 4: Check if HM is in bag
+	ld a, HM_FLY
+	ld [wCurItem], a
+	ld hl, wNumItems
+	call CheckItem
+	ret nc ; .fail, hm isnt in bag
+
+; Step 5: Check if mon can learn move via HM/TM/Move Tutor
+	ld hl, FLY
+	call GetMoveIDFromIndex
+	call CheckMonCanLearn_TM_HM
+	jr c, .yes
+
+; Step 6: Check if Mon can learn move via LVL-UP
+	ld hl, FLY
+	call GetMoveIDFromIndex
+	call CheckLvlUpMoves
+	ret c ; fail
+.yes
+	ld a, MONMENUITEM_FLY
+	call AddMonMenuItem
+	ret
+
+Can_Use_Sweet_Scent:
+; Step 1: Location check
+	farcall CanEncounterWildMon ; CanUseSweetScent instead for older versions of pokecrystal
+	ret nc
+	farcall GetMapEncounterRate
+	ld a, b
+	and a
+	ret z
+
+.valid_location
+; Step 2: Check if mon knows Move 
+	ld hl, SWEET_SCENT
+	call GetMoveIDFromIndex
+	call CheckMonKnowsMove
+	and a
+	jr z, .yes
+
+; Step 3: Check if TM is in bag
+	ld a, TM_SWEET_SCENT
+	ld [wCurItem], a
+	ld hl, wNumItems
+	call CheckItem
+	ret nc ; .fail, tm not in bag
+
+; Step 4: Check if mon can learn Move via TM/HM/Move tutor
+	ld hl, SWEET_SCENT
+	call GetMoveIDFromIndex
+	call CheckMonCanLearn_TM_HM
+	jr c, .yes
+
+; Step 5: Check if mon can learn move via LVL-UP
+	ld hl, SWEET_SCENT
+	call GetMoveIDFromIndex
+	call CheckLvlUpMoves
+	ret c ; fail
+.yes
+	ld a, MONMENUITEM_SWEETSCENT
+	call AddMonMenuItem
+	ret
+
+CanUseDig:
+; Step 1: Location Check
+	call GetMapEnvironment
+	cp CAVE
+	jr z, .valid_location
+	cp DUNGEON
+	ret nz ; fail, not inside cave or dungeon
+
+.valid_location
+; Step 2: Check if Mon knows Move
+	ld hl, DIG
+	call GetMoveIDFromIndex
+	call CheckMonKnowsMove
+	and a
+	jr z, .yes
+
+; Step 3: Check if TM/HM is in bag
+	ld a, TM_DIG
+	ld [wCurItem], a
+	ld hl, wNumItems
+	call CheckItem
+	ret nc ; .fail ; TM not in bag
+
+; Step 4: Check if Mon can learn Dig via TM/HM/Move Tutor
+	ld hl, DIG
+	call GetMoveIDFromIndex
+	call CheckMonCanLearn_TM_HM
+	jr c, .yes
+
+; Step 5: Check if Mon can learn move via LVL-UP
+	ld a, DIG
+	call CheckLvlUpMoves
+	ret c ; fail
+.yes
+	ld a, MONMENUITEM_DIG
+	call AddMonMenuItem
+	ret
+
+CanUseTeleport:
+; Step 1: Location Check
+	call GetMapEnvironment
+	call CheckOutdoorMap
+	ret nz ; .fail
+	
+; Step 2: Check if mon knows move
+	ld hl, TELEPORT
+	call GetMoveIDFromIndex
+	call CheckMonKnowsMove
+	and a
+	jr z, .yes
+
+; Step 3: Check if mon learns move via LVL-UP
+	ld hl, TELEPORT
+	call GetMoveIDFromIndex
+	call CheckLvlUpMoves
+	ret c ; fail
+.yes
+	ld a, MONMENUITEM_TELEPORT
+	call AddMonMenuItem	
+	ret
+
+CanUseSoftboiled:
+	ld hl, SOFTBOILED
+	call GetMoveIDFromIndex
+	call CheckMonKnowsMove
+	and a
+	ret nz
+	ld a, MONMENUITEM_SOFTBOILED
+	call AddMonMenuItem
+	ret
+
+CanUseMilkdrink:
+	ld hl, MILK_DRINK
+	call GetMoveIDFromIndex
+	call CheckMonKnowsMove
+	and a
+	ret nz
+
+	ld a, MONMENUITEM_MILKDRINK
+	call AddMonMenuItem
 	ret
