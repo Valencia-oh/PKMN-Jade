a682bc43765c797db0c30afe4ed321efcc66d8ee
Valencia-oh <valencia_oh@protonmail.com>
2025-12-23 16:47:57 -0500

    Create milotic.asm

---
+++ data/pokemon/dex_entries/milotic.asm
@@ -0,0 +1,10 @@
+	db "TENDER@" ; species name
+	dw 2004, 3571 ; height, weight
+
+	db   "Often called the "
+	next "most beautiful"
+	next "#MON in Johto"
+
+	page "many gym leaders"
+	next "sought to capture"
+	next "it, none succeeded.@"
