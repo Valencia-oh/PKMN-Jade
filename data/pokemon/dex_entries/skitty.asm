2723a23abbb51dd62fb3e68ba5055eaad7d27cb2
Valencia-oh <valencia_oh@protonmail.com>
2025-12-23 11:56:53 -0500

    Create skitty.asm

---
+++ data/pokemon/dex_entries/skitty.asm
@@ -0,0 +1,10 @@
+	db "KITTEN@" ; species name
+	dw 200, 243 ; height, weight
+
+	db   "It is a very"
+	next "playful #MON"
+	next "but it easily gets"
+
+	page "into trouble with"
+	next "its trainers due to"
+	next "its youth.@"
