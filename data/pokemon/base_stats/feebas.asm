29667954caaec456fe31a2465a11fcd2712eb5d1
Valencia-oh <valencia_oh@protonmail.com>
2025-12-23 17:08:23 -0500

    Update feebas.asm

--- data/pokemon/base_stats/feebas.asm
+++ data/pokemon/base_stats/feebas.asm
@@ -12,7 +12,7 @@
 	db 20 ; step cycles to hatch
 	INCBIN "gfx/pokemon/feebas/front.dimensions"
 	dw NULL, NULL ; unused (beta front/back pics)
-	db GROWTH_ERRATIC ; growth rate
+	db GROWTH_MEDIUM_SLOW ; growth rate
 	dn EGG_WATER_1, EGG_DRAGON ; egg groups
 
 	; tm/hm learnset
