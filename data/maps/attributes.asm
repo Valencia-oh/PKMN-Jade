18959e266fb69898c1a130c6810e06d206c706b4
Valencia-oh <valencia_oh@protonmail.com>
2025-12-29 15:52:14 -0500

    Update attributes.asm

--- data/maps/attributes.asm
+++ data/maps/attributes.asm
@@ -174,9 +174,7 @@ ENDM
 	map_attributes Route32, ROUTE_32, $05, NORTH | SOUTH | EAST
 	connection north, VioletCity, VIOLET_CITY, 0
 	connection south, Route33, ROUTE_33, 0
-	connection south, AzaleaShore, AZALEA_SHORE, -3
 	connection east, Route47, ROUTE_47, 20
-	connection east, ForgottenPillar, FORGOTTEN_PILLAR, 40
 
 	map_attributes Route33, ROUTE_33, $05, NORTH | WEST | EAST
 	connection north, Route32, ROUTE_32, 0
@@ -397,11 +395,9 @@ ENDM
 
 	map_attributes ForgottenPillar, FORGOTTEN_PILLAR, $2c,  NORTH | WEST 
 	connection north, Route47, ROUTE_47, 9
-	connection west, Route32, ROUTE_32, -40
 	connection west, AzaleaShore, AZALEA_SHORE, 6
 
 	map_attributes AzaleaShore, AZALEA_SHORE, $2c, NORTH | WEST | EAST
-	connection north, Route32, ROUTE_32, 3
 	connection east, ForgottenPillar, FORGOTTEN_PILLAR, -6
 	connection west, Route33, ROUTE_33, 0
 
