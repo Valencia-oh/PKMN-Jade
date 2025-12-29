c0f435f9b31a8fb270ade8fca8f0e3b2262d574d
Valencia-oh <valencia_oh@protonmail.com>
2025-12-29 10:19:32 -0500

    Create altaria.asm

---
+++ data/pokemon/dex_entries/altaria.asm
@@ -0,0 +1,10 @@
+	db "HUMMING@" ; species name
+	dw 307, 454 ; height, weight
+
+	db   "If you hear a"
+	next "beautiful melody"
+	next "trilling deep in"
+
+	page "mountains far from"
+	next "people, it is"
+	next "Altaria's humming.@"
