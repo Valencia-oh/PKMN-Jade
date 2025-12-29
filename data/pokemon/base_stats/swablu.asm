d1951d2049399aab2b76b493d42a9bd2ff6d7dd7
Valencia-oh <valencia_oh@protonmail.com>
2025-12-29 10:15:57 -0500

    Create swablu.asm

---
+++ data/pokemon/base_stats/swablu.asm
@@ -0,0 +1,20 @@
+	db 0 ; species ID placeholder
+
+	db  45,  40, 60,  50,  40, 75
+	evs  0,   0,   0,   0,   0,   3
+	;   hp  atk  def  spd  sat  sdf
+
+	db DRAGON, FLYING ; type
+	db 255 ; catch rate
+	db 74 ; base exp
+	dw NO_ITEM, NO_ITEM ; items
+	db GENDER_UNKNOWN ; gender ratio
+	db 20 ; step cycles to hatch
+	INCBIN "gfx/pokemon/swablu/front.dimensions"
+	dw NULL, NULL ; unused (beta front/back pics)
+	db GROWTH_SLOW ; growth rate
+	dn EGG_FLYING, EGG_DRAGON ; egg groups
+
+	; tm/hm learnset
+	tmhm CURSE, ROAR, TOXIC, ROCK_SMASH, HIDDEN_POWER, SUNNY_DAY, SNORE, BLIZZARD, HYPER_BEAM, ICY_WIND, PROTECT, RAIN_DANCE, ENDURE, FRUSTRATION, RETURN, MUD_SLAP, DOUBLE_TEAM, SWAGGER, SLEEP_TALK, SANDSTORM, SWIFT, DETECT, REST, STEEL_WING, FLY, ICE_BEAM
+	; end
