b008aa6ea02da0e530904311d57073112fe87262
Valencia-oh <valencia_oh@protonmail.com>
2025-12-29 10:17:44 -0500

    Create swablu.asm

---
+++ data/pokemon/dex_entries/swablu.asm
@@ -0,0 +1,10 @@
+	db "COTTON BIRD@" ; species name
+	dw 104, 26 ; height, weight
+
+	db   "It constantly"
+	next "grooms its"
+	next "cotton-like wings"
+
+	page "It takes"
+	next "a shower to clean"
+	next "itself if when dirty.@"
