9965375442a631c0857d1eaa6267cf6f3b74d4c4
Valencia-oh <valencia_oh@protonmail.com>
2025-12-29 14:47:53 -0500

    Update attributes.asm

--- data/maps/attributes.asm
+++ data/maps/attributes.asm
@@ -391,12 +391,12 @@ ENDM
 	connection south, LavenderTown, LAVENDER_TOWN, 0
 
 	map_attributes Route47, ROUTE_47, $2c,  SOUTH | WEST | EAST
-	connection south, ForgottenPillar, FORGOTTEN_PILLAR, -14
+	connection south, ForgottenPillar, FORGOTTEN_PILLAR, -6
 	connection west, Route32, ROUTE_32, -20
 	connection east, CherrygroveCity, CHERRYGROVE_CITY, 7
 
 	map_attributes ForgottenPillar, FORGOTTEN_PILLAR, $2c,  NORTH | WEST 
-	connection north, Route47, ROUTE_47, 14
+	connection north, Route47, ROUTE_47, 6
 	connection west, Route32, ROUTE_32, 5
 	connection west, AzaleaShore, AZALEA_SHORE, 11
 
