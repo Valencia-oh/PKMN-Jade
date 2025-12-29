348d4633b70f7f1eebfa9e6bbed470ab9d37b56e
Valencia-oh <valencia_oh@protonmail.com>
2025-12-29 13:55:57 -0500

    Update attributes.asm

--- data/maps/attributes.asm
+++ data/maps/attributes.asm
@@ -176,14 +176,14 @@ ENDM
 	map_attributes Route32, ROUTE_32, $05, NORTH | SOUTH | EAST
 	connection north, VioletCity, VIOLET_CITY, 0
 	connection south, Route33, ROUTE_33, 0
-	connection south, AzaleaShore, AZALEA_SHORE, 8
+	connection south, AzaleaShore, AZALEA_SHORE, -4
 	connection east, Route47, ROUTE_47, 20
-	connection east, ForgottenPillar, FORGOTTEN_PILLAR, -40
+	connection east, ForgottenPillar, FORGOTTEN_PILLAR, -30
 
 	map_attributes Route33, ROUTE_33, $05, NORTH | WEST | EAST
 	connection north, Route32, ROUTE_32, 0
 	connection west, AzaleaTown, AZALEA_TOWN, 0
-	connection east, AzaleaShore, AZALEA_SHORE, 1
+	connection east, AzaleaShore, AZALEA_SHORE, 2
 
 	map_attributes Route34, ROUTE_34, $05, NORTH | EAST
 	connection north, GoldenrodCity, GOLDENROD_CITY, -5
@@ -399,13 +399,13 @@ ENDM
 
 	map_attributes ForgottenPillar, FORGOTTEN_PILLAR, $2c,  NORTH | WEST 
 	connection north, Route47, ROUTE_47, 4
-	connection west, Route32, ROUTE_32, 40
+	connection west, Route32, ROUTE_32, 30
 	connection west, AzaleaShore, AZALEA_SHORE, -2
 
 	map_attributes AzaleaShore, AZALEA_SHORE, $2c, NORTH | WEST | EAST
-	connection north, Route32, ROUTE_32, 8
+	connection north, Route32, ROUTE_32, 4
 	connection west, ForgottenPillar, FORGOTTEN_PILLAR, 3
-	connection east, Route33, ROUTE_33, 1
+	connection east, Route33, ROUTE_33, 2
 
 	map_attributes GoldenrodIsle, GOLDENROD_ISLE, $2c, WEST | EAST
 	connection west, Route41, ROUTE_41, 0
