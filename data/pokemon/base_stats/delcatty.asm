e05789d439c5c37973c94e18a745fa275b9a1ae8
Valencia-oh <valencia_oh@protonmail.com>
2025-12-29 20:38:03 -0500

    Update delcatty.asm

--- data/pokemon/base_stats/delcatty.asm
+++ data/pokemon/base_stats/delcatty.asm
@@ -4,7 +4,7 @@
 	evs  1,   0,   0,   1,   0,   0
 	;   hp  atk  def  spd  sat  sdf
 
-	db NORMAL, DARK ; type
+	db NORMAL, FAIRY; type
 	db 60 ; catch rate
 	db 140 ; base exp
 	dw MYSTERYBERRY, MOON_STONE ; items
