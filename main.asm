aefbbad0598636d3cddb7712a29a76c45ccd73f4
Valencia-oh <valencia_oh@protonmail.com>
2026-01-02 14:30:43 -0500

    Update main.asm

--- main.asm
+++ main.asm
@@ -703,6 +703,11 @@ SECTION "Fade System", ROMX
 INCLUDE "engine/gfx/fade.asm"
 
 
+SECTION "ETB Abilities", ROMX
+
+INCLUDE "engine/battle/entrance_abilities.asm"
+
+
 SECTION "Stadium 2 Checksums", ROMX[$7DE0], BANK[$7F]
 
 ; The end of the ROM is taken up by checksums of the content, apparently used
