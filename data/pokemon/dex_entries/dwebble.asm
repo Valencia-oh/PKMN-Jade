a6efe362333a502f0f279a9f4493b14acf850b2e
Valencia-oh <valencia_oh@protonmail.com>
2026-01-02 11:30:34 -0500

    Create dwebble.asm

---
+++ data/pokemon/dex_entries/dwebble.asm
@@ -0,0 +1,10 @@
+	db "ROCK INN@" ; species name
+	dw 100, 320 ; height, weight
+
+	db   "FILLER ENTRY"
+	next "FILLER ENTRY"
+	next "FILLER ENTRY"
+
+	page "FILLER ENTRY"
+	next "FILLER ENTRY"
+	next "FILLER ENTRY.@"
