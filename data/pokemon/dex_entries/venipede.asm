8b2b8b591a248ee8c86a57dadaef0d15dea96091
Valencia-oh <valencia_oh@protonmail.com>
2026-01-02 11:31:57 -0500

    Create venipede.asm

---
+++ data/pokemon/dex_entries/venipede.asm
@@ -0,0 +1,10 @@
+	db "CENTIPEDE@" ; species name
+	dw 104, 117 ; height, weight
+
+	db   "FILLER ENTRY"
+	next "FILLER ENTRY"
+	next "FILLER ENTRY"
+
+	page "FILLER ENTRY"
+	next "FILLER ENTRY"
+	next "FILLER ENTRY.@"
