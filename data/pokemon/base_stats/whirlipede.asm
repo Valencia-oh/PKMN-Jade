c8de6b373a32a60f0cd7cbaaafbe55be9e8738bc
Valencia-oh <valencia_oh@protonmail.com>
2026-01-02 12:06:33 -0500

    Create whirlipede.asm

---
+++ data/pokemon/base_stats/whirlipede.asm
@@ -0,0 +1,20 @@
+	db 0 ; species ID placeholder
+
+	db  40,  55,  99,  47,  40,  79
+	evs  0,   0,   2,   0,   0,   0
+	;   hp  atk  def  spd  sat  sdf
+
+	db BUG, POISON ; type
+	db 120 ; catch rate
+	db 126 ; base exp
+	dw NO_ITEM, NO_ITEM ; items
+	db GENDER_F50 ; gender ratio
+	db 15 ; step cycles to hatch
+	INCBIN "gfx/pokemon/whirlipede/front.dimensions"
+	dw NULL, NULL ; unused (beta front/back pics)
+	db GROWTH_MEDIUM_SLOW ; growth rate
+	dn EGG_BUG, EGG_BUG ; egg groups
+
+	; tm/hm learnset
+	tmhm CURSE, TOXIC, HIDDEN_POWER, SUNNY_DAY, SNORE, HYPER_BEAM, PROTECT, GIGA_DRAIN, ENDURE, FRUSTRATION, SOLARBEAM, RETURN, DIG, PSYCHIC_M, DOUBLE_TEAM, SWAGGER, SLEEP_TALK, SLUDGE_BOMB, REST, ATTRACT, THIEF, FLASH
+	; end
