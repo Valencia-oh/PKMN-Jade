850eb9e91033d8b0d64d32e92a0c3ba062386c24
Valencia-oh <valencia_oh@protonmail.com>
2026-01-02 11:34:11 -0500

    Create sculipede.asm

---
+++ data/pokemon/dex_entries/sculipede.asm
@@ -0,0 +1,10 @@
+	db "MEGAPEDE@" ; species name
+	dw 802, 4420 ; height, weight
+
+	db   "FILLER ENTRY"
+	next "FILLER ENTRY"
+	next "FILLER ENTRY"
+
+	page "FILLER ENTRY"
+	next "FILLER ENTRY"
+	next "FILLER ENTRY.@"
