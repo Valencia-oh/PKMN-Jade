aae716749b1bee8a3bb8be276c35ffaeef57f74b
Valencia-oh <valencia_oh@protonmail.com>
2026-01-02 11:31:12 -0500

    Create crustle.asm

---
+++ data/pokemon/dex_entries/crustle.asm
@@ -0,0 +1,10 @@
+	db "STONE HOME@" ; species name
+	dw 407, 4409 ; height, weight
+
+	db   "FILLER ENTRY"
+	next "FILLER ENTRY"
+	next "FILLER ENTRY"
+
+	page "FILLER ENTRY"
+	next "FILLER ENTRY"
+	next "FILLER ENTRY.@"
