44a6d074a37a9e3a63fa6c825f87de0cba068ab4
Valencia-oh <valencia_oh@protonmail.com>
2026-01-02 11:32:44 -0500

    Create whirlipede.asm

---
+++ data/pokemon/dex_entries/whirlipede.asm
@@ -0,0 +1,10 @@
+	db "CURLIPEDE@" ; species name
+	dw 311, 1290 ; height, weight
+
+	db   "FILLER ENTRY"
+	next "FILLER ENTRY"
+	next "FILLER ENTRY"
+
+	page "FILLER ENTRY"
+	next "FILLER ENTRY"
+	next "FILLER ENTRY.@"
