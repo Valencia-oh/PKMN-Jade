d921d101ea96d58cc20095537121a4fb05d3d6a2
Valencia-oh <valencia_oh@protonmail.com>
2026-01-02 20:19:48 -0500

    Update palettes.asm

--- data/pokemon/palettes.asm
+++ data/pokemon/palettes.asm
@@ -644,10 +644,10 @@ INCBIN "gfx/pokemon/pwooper/normal.gbcpal", middle_colors
 INCLUDE "gfx/pokemon/pwooper/shiny.pal"
 INCBIN "gfx/pokemon/clodsire/normal.gbcpal", middle_colors
 INCLUDE "gfx/pokemon/clodsire/shiny.pal"
-INCBIN "gfx/pokemon/flamigo/normal.gbcpal", middle_colors
-INCLUDE "gfx/pokemon/flamigo/shiny.pal"
-INCBIN "gfx/pokemon/farigiraf/normal.gbcpal", middle_colors
-INCLUDE "gfx/pokemon/farigiraf/shiny.pal"
+;INCBIN "gfx/pokemon/flamigo/normal.gbcpal", middle_colors
+;INCLUDE "gfx/pokemon/flamigo/shiny.pal"
+;INCBIN "gfx/pokemon/farigiraf/normal.gbcpal", middle_colors
+;INCLUDE "gfx/pokemon/farigiraf/shiny.pal"
 INCBIN "gfx/pokemon/chi_yu/normal.gbcpal", middle_colors
 INCLUDE "gfx/pokemon/chi_yu/shiny.pal"
 INCBIN "gfx/pokemon/ersrheadbby/normal.gbcpal", middle_colors
