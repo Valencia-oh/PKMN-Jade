16e37ae803b0010b56f4d7947c2b1ccb3d91911b
Valencia-oh <valencia_oh@protonmail.com>
2026-04-24 11:11:46 -0400

    Add dex entry for Farigiraf species

---
+++ data/pokemon/dex_entries/farigiraf.asm
@@ -0,0 +1,10 @@
+	db "LONG NECK@" ; species name
+	dw 1006, 3275 ; height, weight
+
+	db   "FILLER ENTRY"
+	next "FILLER ENTRY"
+	next "FILLER ENTRY"
+
+	page "FILLER ENTRY"
+	next "FILLER ENTRY"
+	next "FILLER ENTRY.@"
