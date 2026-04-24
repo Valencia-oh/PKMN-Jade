8c0bcac99d4fdff0bc43dc14d3a0c2030015f635
Valencia-oh <valencia_oh@protonmail.com>
2026-04-24 11:18:29 -0400

    Add dex entry for Flamigo species

---
+++ data/pokemon/dex_entries/flamigo.asm
@@ -0,0 +1,10 @@
+	db "SYCHRONIZE@" ; species name
+	dw 503, 816 ; height, weight
+
+	db   "FILLER ENTRY"
+	next "FILLER ENTRY"
+	next "FILLER ENTRY"
+
+	page "FILLER ENTRY"
+	next "FILLER ENTRY"
+	next "FILLER ENTRY.@"
