4494aee742dc97204cb912bea7802fddd72a2382
Valencia-oh <valencia_oh@protonmail.com>
2026-04-24 11:17:15 -0400

    Update Flamigo's base stats and properties

---
+++ data/pokemon/base_stats/flamigo.asm
@@ -0,0 +1,20 @@
+	db 0 ; species ID placeholder
+
+	db  82,  115,  74,  90,  75,  64
+	evs  0,   2,   0,   0,   0,   0
+	;   hp  atk  def  spd  sat  sdf
+
+	db FLYING, FIGHTING ; type
+	db 100 ; catch rate
+	db 175 ; base exp
+	dw NO_ITEM, NO_ITEM ; items
+	db GENDER_F50 ; gender ratio
+	db 20 ; step cycles to hatch
+	INCBIN "gfx/pokemon/flamigo/front.dimensions"
+	dw NULL, NULL ; unused (beta front/back pics)
+	db GROWTH_MEDIUM_SLOW ; growth rate
+	dn EGG_FLYING, EGG_FLYING ; egg groups
+
+	; tm/hm learnset
+	tmhm HEADBUTT, CURSE, TOXIC, ZAP_CANNON, ROCK_SMASH, PSYCH_UP, HIDDEN_POWER, SUNNY_DAY, SNORE, PROTECT, ENDURE, FRUSTRATION, IRON_TAIL, THUNDER, EARTHQUAKE, RETURN, PSYCHIC_M, SHADOW_BALL, MUD_SLAP, DOUBLE_TEAM, SWAGGER, SLEEP_TALK, SWIFT, DREAM_EATER, REST, ATTRACT, THIEF, NIGHTMARE, STRENGTH, THUNDERBOLT, FLY
+	; end
