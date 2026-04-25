a98c438cbb036466e9bcfd2b71b1468a82939f96
Valencia-oh <valencia_oh@protonmail.com>
2026-04-25 12:51:23 -0400

    Add starter choice menu for player selection

--- engine/menus/intro_menu.asm
+++ engine/menus/intro_menu.asm
@@ -622,6 +622,7 @@ endc
 	ld hl, OakText6
 	call PrintText
 	call NamePlayer
+	call StarterChoice
 	ld hl, OakText7
 	jmp PrintText
 
@@ -658,6 +659,56 @@ OakText6:
 OakText7:
 	text_far _OakText7
 	text_end
+
+OakTextStarter:
+	text_far _OakTextStarter
+	text_end
+
+StarterChoice:
+	ld hl, OakTextStarter
+	call PrintText
+	ld hl, .StarterMenuHeader
+	call LoadMenuHeader
+	call VerticalMenu
+	call CloseWindow	
+	ld a, [wMenuCursorY]
+	cp $1
+	jr z, .StartShroomish
+	cp $2
+	jr z, .StartWingull
+	cp $3
+	jr z, .StartTorkoal
+
+.StarterMenuHeader:
+	db MENU_BACKUP_TILES ; flags
+	menu_coords 0, 2, 15, TEXTBOX_Y - 1
+	dw .StarterMenuData
+	db 1 ; default option
+
+.StarterMenuData:
+	db STATICMENU_CURSOR ; flags
+	db 3 ; items
+	db "Shroomish@"
+	db "Wingull@"
+	db "Torkoal@"
+
+.StartShroomish:
+	ld de, EVENT_SHROOMISH_CHOICE
+    ld b, SET_FLAG
+	call EventFlagAction
+	ret
+
+.StartWingull:
+	ld de, EVENT_WINGULL_CHOICE
+    ld b, SET_FLAG
+	call EventFlagAction
+	ret
+
+.StartTorkoal:
+	ld de, EVENT_TORKOAL_CHOICE
+    ld b, SET_FLAG
+	call EventFlagAction
+	ret
 
 NamePlayer:
 	farcall MovePlayerPicRight
