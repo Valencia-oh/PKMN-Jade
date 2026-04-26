096777c4677c9ed3f2edf3b17f129dffb08eaed1
Valencia-oh <valencia_oh@protonmail.com>
2026-04-25 21:01:03 -0400

    Delete Elm item giving scripts and texts
    Removed multiple Elm-related scripts and associated text for item giving.

--- maps/ElmsLab.asm
+++ maps/ElmsLab.asm
@@ -103,58 +103,6 @@ ElmWaitingEggHatchScript:
 	closetext
 	end
 
-ShowElmTogepiScript:
-	writetext ShowElmTogepiText1
-	waitbutton
-	closetext
-	showemote EMOTE_SHOCK, ELMSLAB_ELM, 15
-	setevent EVENT_SHOWED_TOGEPI_TO_ELM
-	opentext
-	writetext ShowElmTogepiText2
-	promptbutton
-	writetext ShowElmTogepiText3
-	promptbutton
-ElmGiveEverstoneScript:
-	writetext ElmGiveEverstoneText1
-	promptbutton
-	verbosegiveitem EVERSTONE
-	iffalse ElmScript_NoRoomForEverstone
-	writetext ElmGiveEverstoneText2
-	waitbutton
-	closetext
-	setevent EVENT_GOT_EVERSTONE_FROM_ELM
-	end
-
-ElmScript_CallYou:
-	writetext ElmText_CallYou
-	waitbutton
-ElmScript_NoRoomForEverstone:
-	closetext
-	end
-
-ElmGiveMasterBallScript:
-	writetext ElmGiveMasterBallText1
-	promptbutton
-	verbosegiveitem MASTER_BALL
-	verbosegiveitem MEW_DNA
-	iffalse .notdone
-	setevent EVENT_GOT_MASTER_BALL_FROM_ELM
-	writetext ElmGiveMasterBallText2
-	waitbutton
-.notdone
-	closetext
-	end
-
-ElmGiveTicketScript:
-	writetext ElmGiveTicketText1
-	promptbutton
-	verbosegiveitem S_S_TICKET
-	setevent EVENT_GOT_SS_TICKET_FROM_ELM
-	writetext ElmGiveTicketText2
-	waitbutton
-	closetext
-	end
-
 ElmJumpBackScript1:
 	closetext
 	readvar VAR_FACING
@@ -362,54 +310,6 @@ ElmThoughtEggHatchedText:
 
 	para "Where is the"
 	line "#MON?"
-	done
-
-ElmGiveMasterBallText1:
-	text "ELM: Hi, <PLAY_G>!"
-	line "Thanks to you, my"
-
-	para "research is going"
-	line "great!"
-
-	para "Take these as"
-	line "tokens of my"
-	cont "appreciation."
-	done
-
-ElmGiveMasterBallText2:
-	text "The MASTER BALL is"
-	line "the best!"
-
-	para "It's the ultimate"
-	line "BALL! It'll catch"
-
-	para "any #MON with-"
-	line "out fail."
-
-	para "It's given only to"
-	line "recognized #MON"
-	cont "researchers."
-
-	para "I think you can"
-	line "make much better"
-
-	para "use of it than I"
-	line "can, <PLAY_G>!"
-
-	para "But that's not all"
-	line "this good is said "
-
-	para "to belong to a"
-	line "very rare #MON!"
-
-	para "and that whoever"
-	line "has it, will be!"
-
-	para "able to summon it"
-	line "from it's hiding!"
-
-	para "place near the"
-	line "southern seas!"
 	done
 
 AideText_GiveYouBalls:
